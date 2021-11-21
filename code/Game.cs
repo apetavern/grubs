b423b44e0a8fcb606927c3cb6c0454da0187f8ff
trundler-dev <57276947+trundler-dev@users.noreply.github.com>
2021-11-21 11:20:34 -0400

    Add .gitignore, .addon, and Game class

---
+++ code/Game.cs
@@ -0,0 +1,16 @@
+namespace TerryForm
+{
+	public partial class Game : Sandbox.Game
+	{
+
+		public static Game Instance
+		{
+			get => Current as Game;
+		}
+		
+		public Game()
+		{
+			
+		}
+	}
+}
