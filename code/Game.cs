7037d621c1e6b71ad9824329b4f252e106670ad1
Alex <alex@gu3.me>
2021-11-21 17:33:19 +0000

    Fix Game.cs too

--- code/Game.cs
+++ code/Game.cs
@@ -2,15 +2,11 @@ namespace TerryForm
 {
 	public partial class Game : Sandbox.Game
 	{
+		public static Game Instance => Current as Game;
 
-		public static Game Instance
-		{
-			get => Current as Game;
-		}
-		
 		public Game()
 		{
-			
+
 		}
 	}
 }
