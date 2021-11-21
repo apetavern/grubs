cbd3aabb95fa1af8f4fb6a4549546a53caf9ac23
Alex <alex@gu3.me>
2021-11-21 17:29:58 +0000

    Linter Test 1

---
+++ code/Player.cs
@@ -0,0 +1,18 @@
+﻿namespace TerryForm
+{
+	public class Player : Sandbox.Player
+	{
+		public string lintTest { get; set; }
+
+		public void Test() {
+			/*
+			 * This is deliberately misformatted to see if the linter actually
+			 * catches it
+			 */
+
+			bool TestBool = false;
+			Log.Trace( TestBool );
+		}
+	}
+}
+
