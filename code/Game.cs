d227f6fb4a6eadba9613716a366b40384f3f7e67
Alex <alex@gu3.me>
2021-11-21 17:39:20 +0000

    Format Game.cs

--- code/Game.cs
+++ code/Game.cs
@@ -2,15 +2,14 @@ namespace TerryForm
 {
 	public partial class Game : Sandbox.Game
 	{
-
 		public static Game Instance
 		{
 			get => Current as Game;
 		}
-		
+
 		public Game()
 		{
-			
+
 		}
 	}
 }
