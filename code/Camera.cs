83c82bc93cabb59c2dbc1324996ba5e9e070b449
Alex <alex@gu3.me>
2021-11-21 20:46:33 +0000

    Basic camera & player setup

---
+++ code/Camera.cs
@@ -0,0 +1,35 @@
+﻿using Sandbox;
+
+namespace TerryForm
+{
+	/// <summary>
+	/// Basic 2.5D camera
+	/// </summary>
+	public class Camera : Sandbox.Camera
+	{
+		public override void Activated()
+		{
+			Position = default;
+			Rotation = default;
+		}
+
+		private float Distance => 1024f;
+
+		public override void Update()
+		{
+			var pawn = Local.Pawn;
+			if ( pawn == null ) return;
+
+			// TODO: Check if the pawn is outside the camera's bounds (w/ a 'safe area') and recenter
+
+			// Basic camera
+			var targetPosition = pawn.EyePos + Vector3.Right * Distance;
+			Position = Position.LerpTo( targetPosition, 5 * Time.Delta );
+
+			var lookDir = (pawn.EyePos - targetPosition).Normal;
+			Rotation = Rotation.LookAt( lookDir, Vector3.Up );
+
+			Viewer = pawn;
+		}
+	}
+}
--- code/Player.cs
+++ code/Player.cs
@@ -6,8 +6,9 @@ namespace TerryForm
 	{
 		public override void Respawn()
 		{
-			Controller = new NoclipController();
-			Camera = new FirstPersonCamera();
+			// Need a custom controller!!
+			Controller = new WalkController();
+			Camera = new Camera();
 
 			base.Respawn();
 		}
@@ -15,6 +16,8 @@ namespace TerryForm
 		public override void Simulate( Client cl )
 		{
 			base.Simulate( cl );
+
+			DebugOverlay.Sphere( EyePos, 32f, Color.Yellow, false ); // Visualise the pawn since we don't have a model for it yet
 		}
 
 		public override void OnKilled()
