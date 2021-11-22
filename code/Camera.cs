ecf8e921748622401a916818c66c9e0ed610bda7
Alex <alex@gu3.me>
2021-11-22 16:08:28 +0000

    Make camera moveable

--- code/Camera.cs
+++ code/Camera.cs
@@ -19,21 +19,26 @@ namespace TerryForm
 		private float Distance { get; set; } = 1024f;
 		private float DistanceScrollRate => 128f;
 
+		public Vector3 Center { get; set; }
+
 		public override void Update()
 		{
 			var pawn = Local.Pawn;
 			if ( pawn == null ) return;
 
-			// TODO: Check if the pawn is outside the camera's bounds (w/ a 'safe area') and recenter
-
 			Distance += -Input.MouseWheel * DistanceScrollRate;
 			Distance = DistanceRange.Clamp( Distance );
 
-			var targetPosition = pawn.EyePos + Vector3.Right * Distance;
+			var targetPosition = Center + Vector3.Right * Distance;
 			Position = Position.LerpTo( targetPosition, 5 * Time.Delta );
 
-			var lookDir = (pawn.EyePos - Position).Normal;
+			var lookDir = (Center - targetPosition).Normal;
 			Rotation = Rotation.LookAt( lookDir, Vector3.Up );
+
+			if ( Input.Down( InputButton.Attack2 ))
+			{
+				Center += new Vector3( -Mouse.Delta.x, 0, Mouse.Delta.y );
+			}
 
 			Viewer = pawn;
 		}
