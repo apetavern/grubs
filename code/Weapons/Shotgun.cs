3e4e00c52e89f25090fc75e3379fe3fe938fde74
Matt <78914427+Matt9440@users.noreply.github.com>
2021-11-23 21:44:01 +0000

    Feature/weapons (#25)
    * Mark Weapon as abstract. Put Bazooka logic into it's own Weapon. Added virtual method to use for firing.
    
    * Give the player a Bazooka instead of the base weapon.
    
    * Cleanup.
    
    * Shotgun implementation
    
    * Shotgun particle
    
    Co-authored-by: Matt9440 <matt@ninetyfour.dev>

---
+++ code/Weapons/Shotgun.cs
@@ -0,0 +1,25 @@
+﻿using Sandbox;
+using TerryForm.Pawn;
+
+namespace TerryForm.Weapons
+{
+	public class Shotgun : Weapon
+	{
+		public override string WeaponName => "Shotgun";
+		public override string ModelPath => "models/weapons/shotgun/shotgun.vmdl";
+		public override HoldPose HoldPose => HoldPose.Shotgun;
+		public override float PrimaryRate => 2f;
+
+		public override void Fire()
+		{
+			var tempTrace = Trace.Ray( Owner.EyePos, Owner.EyePos + Owner.EyeRot.Forward.Normal * 200 ).Ignore( this ).Run();
+
+			DebugOverlay.Line( tempTrace.StartPos, tempTrace.EndPos );
+		}
+
+		public override void OnFireEffects()
+		{
+			Particles.Create( "particles/pistol_muzzleflash.vpcf", this, "muzzle" );
+		}
+	}
+}
