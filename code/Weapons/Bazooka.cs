04a92e90314a927423da87163b65843454c151ec
ShadowBrian <brian@shadowbrain.net>
2021-11-23 22:26:12 +0100

    recompiled files and fixed hold_L (#24)
    * Bazooka aim pose changes
    
    * default AO and normal?? idk
    
    * Bazooka model and textures
    
    * Shell model and textures
    
    * Bazooka bone fix
    
    * Shell physics capsule setup
    
    * Shotgun aim poses update, uzi aim poses added using old shotgun aim poses
    
    * Bazooka fix
    
    * Shotgun model
    
    * Compiled materials/models
    
    * Worm hold_L fix
    
    * Bazooka model and textures
    
    * Shell model and textures
    
    * Bazooka bone fix
    
    * Shell physics capsule setup
    
    * Bazooka fix
    
    * Shotgun model
    
    * Compiled materials/models
    
    * Worm hold_L fix
    
    * Mark Weapon as abstract. Put Bazooka logic into it's own Weapon. Added virtual method to use for firing.
    
    * Give the player a Bazooka instead of the base weapon.
    
    * Bazooka model and textures
    
    * Shell model and textures
    
    * Bazooka bone fix
    
    * Shell physics capsule setup
    
    * Bazooka fix
    
    * Shotgun model
    
    * Compiled materials/models
    
    * Worm hold_L fix
    
    Co-authored-by: Matt9440 <matt@ninetyfour.dev>

---
+++ code/Weapons/Bazooka.cs
@@ -0,0 +1,24 @@
+﻿using Sandbox;
+using TerryForm.Pawn;
+
+namespace TerryForm.Weapons
+{
+	public class Bazooka : Weapon
+	{
+		public override string WeaponName => "Bazooka";
+		public override string ModelPath => "models/weapons/bazooka/bazooka.vmdl";
+		public override HoldPose HoldPose => HoldPose.Bazooka;
+		public override float PrimaryRate => 2f;
+
+		public override void Fire()
+		{
+			new ExplodingProjectile()
+				.WithRadius( 60 )
+				.SetMaxBounces( 1 )
+				.WithModel( "models/weapons/shell/shell.vmdl" )
+				.FireFrom( Owner.EyePos + Owner.EyeRot.Forward * 70, Owner.EyeRot.Forward.Normal, 50000 );
+		}
+
+
+	}
+}
