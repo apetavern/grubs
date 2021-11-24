6c5b56ec5c7eee1413f45727a994e5b7abbfccbe
Alex <alex@gu3.me>
2021-11-24 14:04:29 +0000

    Basic wind info panel, improvements to game info panel

---
+++ code/UI/WindInfoPanel.cs
@@ -0,0 +1,16 @@
+﻿using Sandbox;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
+
+namespace TerryForm.UI
+{
+	public class WindInfoPanel : Panel
+	{
+		public WindInfoPanel()
+		{
+			StyleSheet.Load( "/Code/UI/WindInfoPanel.scss" );
+
+			Add.Panel( "wind-inner" );
+		}
+	}
+}
