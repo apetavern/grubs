7b58a25fce1814e556d18f32f1a00e50e9dc29f9
Alex <alex@gu3.me>
2021-11-24 23:10:57 +0000

    (Probably bad) attempt to network the current state

--- code/Game.cs
+++ code/Game.cs
@@ -7,7 +7,9 @@ namespace TerryForm
 	public partial class Game : Sandbox.Game
 	{
 		public static Game Instance => Current as Game;
-		public static StateHandler StateHandler { get; private set; }
+		public static StateHandler StateHandler => Instance.InternalStateHandler;
+
+		[Net] public StateHandler InternalStateHandler { get; set; }
 
 		public Game()
 		{
@@ -16,7 +18,7 @@ namespace TerryForm
 			if ( IsClient )
 				return;
 
-			StateHandler = new();
+			InternalStateHandler = new();
 			Event.Register( StateHandler );
 		}
 
