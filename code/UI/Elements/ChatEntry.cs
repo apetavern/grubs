d8a781b2859fbaaca4fb2717da3ca7eeb485a576
Alex <alex@gu3.me>
2021-11-25 12:32:49 +0000

    Custom chat box

---
+++ code/UI/Elements/ChatEntry.cs
@@ -0,0 +1,34 @@
+﻿using Sandbox;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
+
+public partial class ChatEntry : Panel
+{
+	public Label NameLabel { get; internal set; }
+	public Label Message { get; internal set; }
+	public Image Avatar { get; internal set; }
+
+	private RealTimeSince TimeSinceBorn = 0;
+
+	public ChatEntry()
+	{
+		Avatar = Add.Image();
+		NameLabel = Add.Label( "Name", "name" );
+		Message = Add.Label( "Message", "message" );
+	}
+
+	public override void Tick()
+	{
+		base.Tick();
+
+		if ( TimeSinceBorn > 3 && !ChatBox.Current.HasClass( "open" ) )
+		{
+			Hide();
+		}
+	}
+
+	public void Hide()
+	{
+		AddClass( "hide" );
+	}
+}
