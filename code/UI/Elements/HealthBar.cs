2bfe92c2e4838f28cedab8b1ed34e669ca996e00
Alex <alex@gu3.me>
2021-11-25 12:32:57 +0000

    Basic health bar element

---
+++ code/UI/Elements/HealthBar.cs
@@ -0,0 +1,13 @@
+﻿using Sandbox.UI;
+
+namespace TerryForm.UI.Elements
+{
+	public class HealthBar : Panel
+	{
+		public HealthBar()
+		{
+			StyleSheet.Load( "/Code/UI/Elements/HealthBar.scss" );
+			Add.Panel( "health-bar-inner" );
+		}
+	}
+}
