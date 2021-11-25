bb1681c320b9ae9b626ad3e485c312755964f03c
Alex <alex@gu3.me>
2021-11-25 12:14:26 +0000

    Initial player info panel prototyping

---
+++ code/UI/PlayerInfoPanel.cs
@@ -0,0 +1,20 @@
+﻿using Sandbox;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
+
+namespace TerryForm.UI
+{
+	internal class PlayerInfoPanel : Panel
+	{
+		public PlayerInfoPanel()
+		{
+			StyleSheet.Load( "/Code/UI/PlayerInfoPanel.scss" );
+
+			// Random background for stylistic purposes
+			AddClass( $"background-{Rand.Int( 0, 3 )}" );
+
+			Add.Image( "avatar:76561198128972602", "player-avatar" );
+			Add.Label( "Player Name", "player-name" );
+		}
+	}
+}
