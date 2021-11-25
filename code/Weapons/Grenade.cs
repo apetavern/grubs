866212e60d35762b62902b2dc4f6d1f35ada7ca5
trundler-dev <57276947+trundler-dev@users.noreply.github.com>
2021-11-25 16:35:33 -0400

    Adding grenade and railgun

---
+++ code/Weapons/Grenade.cs
@@ -0,0 +1,23 @@
+﻿using TerryForm.Pawn;
+
+namespace TerryForm.Weapons
+{
+	public class Grenade : Weapon
+	{
+		public override string WeaponName => "Grenade";
+		public override string ModelPath => "models/weapons/grenade/grenade.vmdl";
+		public override HoldPose HoldPose => HoldPose.Throwable;
+		public override float PrimaryRate => 2f;
+
+		public override void Fire()
+		{
+			new ExplodingProjectile()
+				.WithRadius( 60 )
+				.SetMaxBounces( 4 )
+				.WithModel( ModelPath )
+				.FireFrom( Owner.EyePos + Owner.EyeRot.Forward * 70, Owner.EyeRot.Forward.Normal, 50000 );
+
+			base.Fire();
+		}
+	}
+}
