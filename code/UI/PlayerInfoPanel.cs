0a046e64297f8460e1ad20235205db3416a0b88d
Alex <alex@gu3.me>
2021-11-25 12:59:30 +0000

    Better layout for players, use template

--- code/UI/PlayerInfoPanel.cs
+++ code/UI/PlayerInfoPanel.cs
@@ -5,19 +5,12 @@ using TerryForm.UI.Elements;
 
 namespace TerryForm.UI
 {
-	internal class PlayerInfoPanel : Panel
+	[UseTemplate]
+	public class PlayerInfoPanel : Panel
 	{
 		public PlayerInfoPanel()
 		{
 			StyleSheet.Load( "/Code/UI/PlayerInfoPanel.scss" );
-
-			// Random background for stylistic purposes
-			AddClass( $"background-{Rand.Int( 0, 3 )}" );
-
-			Add.Image( "avatar:76561198128972602", "player-avatar" );
-			Add.Label( "Player Name", "player-name" );
-
-			AddChild<HealthBar>();
 		}
 	}
 }
