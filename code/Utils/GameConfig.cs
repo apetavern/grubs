ccab7f22dab5f4d5055b11bb706206f64b8045f3
Matt9440 <78914427+Matt9440@users.noreply.github.com>
2021-11-26 08:32:28 +0000

    Increase WormCount to 2

--- code/Utils/GameConfig.cs
+++ code/Utils/GameConfig.cs
@@ -2,7 +2,7 @@
 {
 	public static class GameConfig
 	{
-		public static int WormCount { get; set; } = 1;
+		public static int WormCount { get; set; } = 2;
 		public static int TurnDurationSeconds { get; set; } = 45;
 		public static int TurnTimeRemainingAfterFired { get; set; } = 5;
 		public static int MinimumPlayersToStart { get; set; } = 2;
