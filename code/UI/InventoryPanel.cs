0575d62aad026e5a79e9090128dfa0a9b98569c2
Alex <alex@gu3.me>
2021-11-26 23:25:28 +0000

    Initial inventory panel

---
+++ code/UI/InventoryPanel.cs
@@ -0,0 +1,14 @@
+﻿using Sandbox;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
+
+namespace TerryForm.UI
+{
+	public class InventoryPanel : Panel
+	{
+		public InventoryPanel()
+		{
+			StyleSheet.Load( "/Code/UI/InventoryPanel.scss" );
+		}
+	}
+}
