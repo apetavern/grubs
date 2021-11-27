0d7621f0c4f08a97bfb545c86643cbec5c55960d
Alex <alex@gu3.me>
2021-11-27 16:42:13 +0000

    Hook things up to the actual game

--- code/UI/PlayerInfoPanel.cs
+++ code/UI/PlayerInfoPanel.cs
@@ -1,16 +1,37 @@
 ﻿using Sandbox;
 using Sandbox.UI;
-using Sandbox.UI.Construct;
-using TerryForm.UI.Elements;
+using TerryForm.States;
 
 namespace TerryForm.UI
 {
 	[UseTemplate]
 	public class PlayerInfoPanel : Panel
 	{
-		public PlayerInfoPanel()
+		public Label PlayerNameLabel { get; set; }
+		public Image PlayerAvatarImage { get; set; }
+
+		public PlayerInfoPanel( Client client )
 		{
 			StyleSheet.Load( "/Code/UI/PlayerInfoPanel.scss" );
+
+			PlayerNameLabel.Text = client.Name;
+			PlayerAvatarImage.SetTexture( $"avatar:{client.PlayerId}" );
+
+			BindClass( "my-turn", () =>
+			{
+				var game = Game.Instance;
+				if ( game == null ) return false;
+
+				var state = game.StateHandler.State;
+				if ( state == null ) return false;
+
+				if ( state is PlayingState { Turn: { ActivePlayer: Pawn.Player player } } )
+				{
+					return player.Client == client;
+				}
+
+				return false;
+			} );
 		}
 	}
 }
