5409a95ccfa225ed33fdf5746224135f1379c837
Matt9440 <78914427+Matt9440@users.noreply.github.com>
2021-11-28 13:55:38 +0000

    Don't simulate if we have 0 segments.

--- code/Weapons/Projectiles/Projectile.cs
+++ code/Weapons/Projectiles/Projectile.cs
@@ -29,7 +29,7 @@ namespace TerryForm.Weapons
 		public override void Simulate( Client cl )
 		{
 			// This might be shite
-			if ( Segments is null ) return;
+			if ( Segments is null || !Segments.Any() ) return;
 			if ( IsCompleted == true ) return;
 
 			if ( Position.IsNearlyEqual( Segments[0].EndPos, 0.1f ) )
