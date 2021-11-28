8a82c8c72c672348436bc5dd725632768fa74662
Matt9440 <78914427+Matt9440@users.noreply.github.com>
2021-11-28 13:44:37 +0000

    Added HudEntity Instance. Added method to block input passthrough to world.

--- code/UI/HudEntity.cs
+++ code/UI/HudEntity.cs
@@ -3,16 +3,26 @@ using TerryForm.UI.World;
 
 namespace TerryForm.UI
 {
-	internal class HudEntity : Sandbox.HudEntity<RootPanel>
+	public class HudEntity : Sandbox.HudEntity<RootPanel>
 	{
+		public static HudEntity Instance { get; set; }
+		private bool ShouldReceiveInput { get; set; } = false;
+
 		public HudEntity()
 		{
 			if ( IsClient )
 			{
+				Instance = this;
 				RootPanel.SetTemplate( "/Code/UI/HudEntity.html" );
 
 				_ = new WormNametags();
 			}
+		}
+
+		public void ReceiveInput( bool receiveInput )
+		{
+			ShouldReceiveInput = receiveInput;
+			RootPanel.SetClass( "ReceiveInput", ShouldReceiveInput );
 		}
 	}
 }
