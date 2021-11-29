c207e5f09b2acc74b847f57ce3246dafcffd6233
Johan <Leafdroids@gmail.com>
2021-11-29 19:27:09 +0100

    initial commit i think
    console commands
    terrain_gen - generate all chunks
    terrain_clear - clear noise map

---
+++ code/Terrain/EdgeVertex.cs
@@ -0,0 +1,29 @@
+﻿#region Assembly Sandbox.Game, Version=1.0.1.0, Culture=neutral, PublicKeyToken=null
+// C:\Program Files (x86)\Steam\steamapps\common\sbox\bin\managed\Sandbox.Game.dll
+#endregion
+using Sandbox;
+using System.Runtime.InteropServices;
+
+namespace Sandbox
+{
+	[StructLayout( LayoutKind.Sequential )]
+	public struct EdgeVertex
+    {
+		public Vector3 position;
+		public Vector3 normal;
+		public Color32 color;
+
+		public static readonly VertexAttribute[] Layout =
+		{
+			new VertexAttribute( VertexAttributeType.Position, VertexAttributeFormat.Float32, 3 ),
+			new VertexAttribute( VertexAttributeType.Normal, VertexAttributeFormat.Float32, 3 ),
+			new VertexAttribute( VertexAttributeType.Color, VertexAttributeFormat.UInt8, 4 )
+		};
+		public EdgeVertex( Vector3 position, Vector3 normal, Color32 color )
+		{
+			this.position = position;
+			this.normal = normal;
+			this.color = color;
+		}
+    }
+}
---
+++ code/Terrain/SDF.cs
@@ -0,0 +1,34 @@
+﻿using Sandbox;
+using System.Linq;
+using Sandbox.UI;
+using System;
+using System.Collections.Generic;
+
+public struct SDF
+{
+	public static float Sphere( Vector3 center, float radius, Vector3 point )
+	{
+		return center.Distance(point) - radius;
+	}
+
+	public static float Lerp( float a, float b, float t )
+	{
+		return t * a + (1f - t) * b;
+	}
+
+	public static float Clamp( float v, float min, float max )
+	{
+		if ( v > max )
+			return max;
+		if ( v < min )
+			return min;
+		return v;
+	}
+
+	public static float SmoothMin( float a, float b, float k = 32 )
+	{
+		float res = MathF.Exp( -k * a ) + MathF.Exp( -k * b );
+		return -MathF.Log( MathF.Max( 0.0001f, res ) ) / k;
+	}
+}
+
