59589ff67f2c105f85d1a8fcd6f76bc012c2f9d8
trundler-dev <57276947+trundler-dev@users.noreply.github.com>
2021-11-30 09:07:28 -0400

    Terrain Generation Base (#70)
    * initial commit i think
    
    console commands
    terrain_gen - generate all chunks
    terrain_clear - clear noise map
    
    * fromatted
    
    * Remove headers, cleanup using statements.
    
    * Complete requested changes. Added Boomer weapon. Disabled debug lines/spheres.
    
    Co-authored-by: Johan <Leafdroids@gmail.com>
    Co-authored-by: Matt9440 <78914427+Matt9440@users.noreply.github.com>

---
+++ code/Terrain/Deformation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Sandbox;
+using System;
+
+public struct Deformation
+{
+	public Vector3 Position { get; private set; }
+	public bool Boolean { get; private set; }
+	public float Radius { get; private set; }
+
+	public Deformation( Vector3 position, bool boolean, float radius )
+	{
+		Position = position;
+		Boolean = boolean;
+		Radius = radius;
+	}
+
+	public override string ToString()
+	{
+		return $"Deformation [{Position}, {Boolean}, {Radius}]";
+	}
+}
---
+++ code/Terrain/EdgeVertex.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Sandbox
+{
+	[StructLayout( LayoutKind.Sequential )]
+	public struct EdgeVertex
+	{
+		public Vector3 position;
+		public Vector3 normal;
+		public Color32 color;
+
+		public static readonly VertexAttribute[] Layout =
+		{
+			new VertexAttribute( VertexAttributeType.Position, VertexAttributeFormat.Float32, 3 ),
+			new VertexAttribute( VertexAttributeType.Normal, VertexAttributeFormat.Float32, 3 ),
+			new VertexAttribute( VertexAttributeType.Color, VertexAttributeFormat.UInt8, 4 )
+		};
+		public EdgeVertex( Vector3 position, Vector3 normal, Color32 color )
+		{
+			this.position = position;
+			this.normal = normal;
+			this.color = color;
+		}
+	}
+}
