36458173c7cb926df767faa7acf43a4b29104e7b
Matt9440 <78914427+Matt9440@users.noreply.github.com>
2021-12-01 08:28:19 +0000

    Give shotgun unlimited ammo for testing

--- code/Utils/GameConfig.cs
+++ code/Utils/GameConfig.cs
@@ -30,7 +30,7 @@ namespace TerryForm.Utils
 			{ "Bazooka", -1 },
 			{ "Grenade", -1 },
 			{ "Railgun", 0 },
-			{ "Shotgun", 2 },
+			{ "Shotgun", -1 },
 			{ "Dynamite", 1 },
 			{ "Boomer", -1 },
 		};
