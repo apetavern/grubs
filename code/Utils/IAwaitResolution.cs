cfa1f018d3c564b1ffb30f9f1ee3287923661201
Matt9440 <78914427+Matt9440@users.noreply.github.com>
2021-12-02 10:51:33 +0000

    IAwaitResolution

---
+++ code/Utils/IAwaitResolution.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerryForm.Utils
+{
+	public interface IAwaitResolution
+	{
+		public bool IsResolved { get; set; }
+	}
+}
