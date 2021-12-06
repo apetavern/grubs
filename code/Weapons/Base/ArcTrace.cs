ac56914dd3f9ecd8d5271d7754c0cac939e97d39
Matt9440 <78914427+Matt9440@users.noreply.github.com>
2021-12-06 11:06:21 +0000

    RunTo() RunTowards() Removed some stuff from ctor

--- code/Weapons/Base/ArcTrace.cs
+++ code/Weapons/Base/ArcTrace.cs
@@ -12,26 +12,24 @@ namespace TerryForm.Weapons
 	public class ArcTrace
 	{
 		public Vector3 StartPos { get; set; }
-		public Vector3 Direction { get; set; }
 		public Vector3 EndPos { get; set; }
-		public float WindForce { get; set; }
-		private float Force { get; set; }
 		private int SegmentCount { get; set; } = 80;
 		public List<ArcSegment> Segments { get; set; } = new();
 
-		public ArcTrace( Vector3 startPos, Vector3 direction, float force, float wind )
+		public ArcTrace( Vector3 startPos )
 		{
 			StartPos = startPos;
-			Direction = direction;
-			Force = force;
-			WindForce = wind;
 		}
 
-		public List<ArcSegment> Run()
+		/// <summary>
+		/// Run a trace with a specific pre-determined end position.
+		/// </summary>
+		public List<ArcSegment> RunTo( Vector3 EndPos )
 		{
-			float epsilon = 0.001f;
-
-			Vector3 velocity = Force * -Direction;
+			//float epsilon = 0.001f;
+			float force = 1;
+			Vector3 direction = EndPos - StartPos;
+			Vector3 velocity = force * -direction;
 			Vector3 position = StartPos;
 
 			for ( int i = 0; i < SegmentCount; i++ )
@@ -39,7 +37,48 @@ namespace TerryForm.Weapons
 				ArcSegment segment = new();
 				segment.StartPos = position;
 
-				velocity -= new Vector3( WindForce, 0, 0 );
+				//velocity -= PhysicsWorld.Gravity * epsilon;
+				position -= velocity;
+
+				segment.EndPos = position;
+
+				var tr = Trace.Ray( segment.StartPos, segment.EndPos ).Radius( 2f ).WorldOnly().Run();
+
+				if ( tr.Hit )
+				{
+					EndPos = tr.EndPos;
+
+					segment.EndPos = EndPos;
+					Segments.Add( segment );
+
+					break;
+				}
+
+				Segments.Add( segment );
+			}
+
+			Draw( Segments );
+			DebugOverlay.Line( EndPos, EndPos + Vector3.Up * 100f, Color.Red, 0, false );
+
+			return new List<ArcSegment>();
+		}
+
+		/// <summary>
+		/// Run a trace specifying the direction, force and wind force.
+		/// </summary>
+		public List<ArcSegment> RunTowards( Vector3 direction, float force, float windForceX )
+		{
+			float epsilon = 0.001f;
+
+			Vector3 velocity = force * -direction;
+			Vector3 position = StartPos;
+
+			for ( int i = 0; i < SegmentCount; i++ )
+			{
+				ArcSegment segment = new();
+				segment.StartPos = position;
+
+				velocity -= new Vector3( windForceX, 0, 0 );
 				velocity -= PhysicsWorld.Gravity * epsilon;
 				position -= velocity;
 
