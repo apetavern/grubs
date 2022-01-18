1e797de704345ae6dfb217bb3d78da6aee30d11a
trundler-dev <57276947+trundler-dev@users.noreply.github.com>
2022-01-18 19:23:20 -0400

    Prototyping for menu

---
+++ code/UI/Menu/MenuPanel.cs
@@ -0,0 +1,14 @@
+﻿using Sandbox.UI;
+
+namespace Grubs.UI.Menu
+{
+	[UseTemplate]
+	public partial class MenuPanel : Panel
+	{
+
+		public MenuPanel()
+		{
+			StyleSheet.Load( "/UI/Menu/MenuPanel.scss" );
+		}
+	}
+}
