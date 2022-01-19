35e81c3f19abbd9b27dc03d2410529bae512cf00
trundler-dev <57276947+trundler-dev@users.noreply.github.com>
2022-01-18 21:21:20 -0400

    Started playerlist, stopping for tonight

---
+++ code/UI/Menu/PlayerList.cs
@@ -0,0 +1,16 @@
+﻿using Sandbox.UI;
+using Sandbox.UI.Construct;
+
+namespace Grubs.UI.Menu
+{
+	[UseTemplate]
+	public partial class PlayerList : Panel
+	{
+		public Label playerName;
+
+		public PlayerList()
+		{
+			StyleSheet.Load( "/UI/Menu/PlayerList.scss" );
+		}
+	}
+}
