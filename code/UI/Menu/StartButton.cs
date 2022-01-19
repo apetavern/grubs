93b8c1bdc0242b33d7d285836dd25731a79cf8f7
trundler-dev <57276947+trundler-dev@users.noreply.github.com>
2022-01-19 16:16:30 -0400

    Start button

---
+++ code/UI/Menu/StartButton.cs
@@ -0,0 +1,13 @@
+﻿using Sandbox.UI;
+
+namespace Grubs.UI.Menu
+{
+	[UseTemplate]
+	public partial class StartButton : Panel
+	{
+		public StartButton()
+		{
+			StyleSheet.Load( "/UI/Menu/StartButton.scss" );
+		}
+	}
+}
