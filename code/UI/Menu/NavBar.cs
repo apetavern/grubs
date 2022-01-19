79787aa60d1474200eec3893520a73f94456a77a
trundler-dev <57276947+trundler-dev@users.noreply.github.com>
2022-01-19 14:36:51 -0400

    Re-write menu to actually look decent

---
+++ code/UI/Menu/NavBar.cs
@@ -0,0 +1,13 @@
+﻿using Sandbox.UI;
+
+namespace Grubs.UI.Menu
+{
+	[UseTemplate]
+	public partial class NavBar : Panel
+	{
+		public NavBar()
+		{
+			StyleSheet.Load( "/UI/Menu/Navbar.scss" );
+		}
+	}
+}
--- code/UI/Menu/PlayerList.cs
+++ code/UI/Menu/PlayerList.cs
@@ -3,11 +3,21 @@ using Sandbox.UI.Construct;
 
 namespace Grubs.UI.Menu
 {
+	public partial class PlayerEntry : Panel
+	{
+		public Image Avatar { get; set; }
+		public Label PlayerName { get; set; }
+
+		public PlayerEntry()
+		{
+			Avatar = Add.Image( "", "avatar" );
+			PlayerName = Add.Label( "Name", "name" );
+		}
+	}
+
 	[UseTemplate]
 	public partial class PlayerList : Panel
 	{
-		public Label playerName;
-
 		public PlayerList()
 		{
 			StyleSheet.Load( "/UI/Menu/PlayerList.scss" );
