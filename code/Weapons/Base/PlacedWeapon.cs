dc01a623ed4384bcce546435e2c25466ba8cd453
Matt9440 <78914427+Matt9440@users.noreply.github.com>
2022-01-21 22:44:00 +0000

    PlacedWeapon

---
+++ code/Weapons/Base/PlacedWeapon.cs
@@ -0,0 +1,31 @@
+﻿using Sandbox;
+using Grubs.Pawn;
+using Grubs.States.SubStates;
+using Grubs.Utils;
+using Grubs.Terrain;
+
+namespace Grubs.Weapons
+{
+	public abstract partial class PlacedWeapon : Weapon
+	{
+		// Weapon settings
+		public override string WeaponName => "";
+		public override string ModelPath => "";
+		public override bool IsFiredTurnEnding => true;
+		public override HoldPose HoldPose => HoldPose.Throwable;
+
+		/// <summary>
+		/// What happens when you actually fire the weapon.
+		/// </summary>
+		protected override void Fire()
+		{
+			Log.Info( "Placed" );
+		}
+
+		[ClientRpc]
+		public virtual void OnFireEffects()
+		{
+			// Do something?
+		}
+	}
+}
--- code/Weapons/Dynamite.cs
+++ code/Weapons/Dynamite.cs
@@ -2,13 +2,10 @@
 
 namespace Grubs.Weapons
 {
-	// ArcWeapon doesn't seem to fit the purpose of weapons with projectiles that aren't thrown.
-	// This should be changed to something like PlacedWeapon.
-	public class Dynamite : ArcWeapon
+	public class Dynamite : PlacedWeapon
 	{
 		public override string WeaponName => "Dynamite";
 		public override string ModelPath => "models/weapons/dynamite/dynamite.vmdl";
-		public override string ProjectileModel => ModelPath;
-		public override HoldPose HoldPose => HoldPose.Throwable;
+		public override HoldPose HoldPose => HoldPose.Droppable;
 	}
 }
