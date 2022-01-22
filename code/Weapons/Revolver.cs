7e64a3ae22f945a47d4ac67052446c51a7c71a41
Matt9440 <78914427+Matt9440@users.noreply.github.com>
2022-01-22 15:33:41 +0000

    Revoler

---
+++ code/Weapons/Revolver.cs
@@ -0,0 +1,11 @@
+﻿using Grubs.Pawn;
+
+namespace Grubs.Weapons
+{
+	public class Revolver : Weapon
+	{
+		public override string WeaponName => "Revolver";
+		public override string ModelPath => "models/weapons/sixshooter/sixshooter.vmdl";
+		public override HoldPose HoldPose => HoldPose.Revolver;
+	}
+}
