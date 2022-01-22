28f6ef1c581a76bb5bddf4c8f3880496b73816d1
Matt9440 <78914427+Matt9440@users.noreply.github.com>
2022-01-22 15:33:19 +0000

    LandMine

---
+++ code/Weapons/LandMine.cs
@@ -0,0 +1,11 @@
+﻿using Grubs.Pawn;
+
+namespace Grubs.Weapons
+{
+	public class LandMine : PlacedWeapon
+	{
+		public override string WeaponName => "Land Mine";
+		public override string ModelPath => "models/weapons/landmine/landmine.vmdl";
+		public override HoldPose HoldPose => HoldPose.Droppable;
+	}
+}
