bb3a95104cd09b5a288b1042c772d70a69870df9
Matt9440 <78914427+Matt9440@users.noreply.github.com>
2022-01-22 15:33:35 +0000

    PetrolBomb

---
+++ code/Weapons/PetrolBomb.cs
@@ -0,0 +1,12 @@
+﻿using Grubs.Pawn;
+
+namespace Grubs.Weapons
+{
+	public class PetrolBomb : ArcWeapon
+	{
+		public override string WeaponName => "Petrol Bomb";
+		public override string ModelPath => "models/weapons/petrolbomb/petrolbomb.vmdl";
+		public override string ProjectileModel => ModelPath;
+		public override HoldPose HoldPose => HoldPose.Throwable;
+	}
+}
