ae6d1e934ab3fcc8dfab27e708112cdbdbd000a0
Matt9440 <78914427+Matt9440@users.noreply.github.com>
2022-01-22 15:33:27 +0000

    MiniGun

---
+++ code/Weapons/Minigun.cs
@@ -0,0 +1,12 @@
+﻿using Sandbox;
+using Grubs.Pawn;
+
+namespace Grubs.Weapons
+{
+	public class Minigun : Weapon
+	{
+		public override string WeaponName => "Minigun";
+		public override string ModelPath => "models/weapons/minigun/minigun.vmdl";
+		public override HoldPose HoldPose => HoldPose.Minigun;
+	}
+}
