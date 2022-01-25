0073388e62cc680d4c10db1cea9469c42ade7be3
Matt9440 <78914427+Matt9440@users.noreply.github.com>
2022-01-25 08:35:44 +0000

    AirDropHelper

---
+++ code/Weapons/Helpers/AirDropHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using Sandbox;
+using Grubs.Pawn;
+using Grubs.Terrain;
+
+namespace Grubs.Weapons.Helpers
+{
+	public partial class AirDropHelper : Entity
+	{
+		static AirDropHelper Instance { get; set; }
+
+		public static void SummonDrop<T>( Entity droppedEntity, AirDropTravelDirection travelDirection = AirDropTravelDirection.Right )
+		{
+			if ( Instance is null )
+				Instance = new();
+
+			Log.Info( $"Summon aircraft dropping {droppedEntity}" );
+		}
+	}
+
+	public enum AirDropTravelDirection
+	{
+		Left,
+		Right
+	}
+}
