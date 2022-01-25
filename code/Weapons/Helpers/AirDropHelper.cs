b4e5adb927e4ad7b6466acbde862927b5614eb4b
Matt9440 <78914427+Matt9440@users.noreply.github.com>
2022-01-25 08:45:53 +0000

    Implement IAwaitResolution

--- code/Weapons/Helpers/AirDropHelper.cs
+++ code/Weapons/Helpers/AirDropHelper.cs
@@ -2,19 +2,33 @@
 using Sandbox;
 using Grubs.Pawn;
 using Grubs.Terrain;
+using Grubs.Utils;
 
 namespace Grubs.Weapons.Helpers
 {
-	public partial class AirDropHelper : Entity
+	public partial class AirDropHelper : Entity, IAwaitResolution
 	{
 		static AirDropHelper Instance { get; set; }
+		public AnimEntity Plane { get; set; }
+		public bool IsResolved { get; set; }
 
 		public static void SummonDrop( Entity droppedEntity, AirDropTravelDirection travelDirection = AirDropTravelDirection.Right )
 		{
 			if ( Instance is null )
 				Instance = new();
 
+			Instance.Plane = new AnimEntity( "models/weapons/airstrikes/plane.vmdl" );
+
 			Log.Info( $"Summon aircraft dropping {droppedEntity}" );
+
+			// Move this later once the plane movement stuff is in.
+			Instance.IsResolved = true;
+		}
+
+		[Event.Tick.Server]
+		public void Move()
+		{
+			// Movement
 		}
 	}
 
