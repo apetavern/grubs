009ab15c07c16e6224863ef6bce42a770ad0656f
Matt9440 <78914427+Matt9440@users.noreply.github.com>
2022-01-25 08:05:17 +0000

    AirWeapon

---
+++ code/Weapons/Base/AirWeapon.cs
@@ -0,0 +1,28 @@
+﻿using Sandbox;
+using Grubs.Pawn;
+
+namespace Grubs.Weapons
+{
+	public abstract partial class AirWeapon : Weapon
+	{
+		// Weapon settings
+		public override string WeaponName => "";
+		public override string ModelPath => "";
+		public override bool IsFiredTurnEnding => true;
+		public override HoldPose HoldPose => HoldPose.Holdable;
+
+		/// <summary>
+		/// What happens when you actually fire the weapon.
+		/// </summary>
+		protected override void Fire()
+		{
+			Log.Info( "Summon something" );
+		}
+
+		[ClientRpc]
+		public override void OnFireEffects()
+		{
+			// Do something?
+		}
+	}
+}
