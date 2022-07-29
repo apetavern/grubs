468f332671ffd25dca2dd31711022984023001e7
trundler-dev <57276947+trundler-dev@users.noreply.github.com>
2022-07-29 16:29:05 -0300

    Strip grubs down to barebones

--- code/Game.cs
+++ code/Game.cs
@@ -1,66 +1,24 @@
-using Sandbox;
-using Grubs.Pawn;
-using Grubs.States;
-using Grubs.Terrain;
-using Grubs.UI;
-using Grubs.Utils;
+global using Sandbox;
+global using Sandbox.UI;
+global using Sandbox.UI.Construct;
 
-namespace Grubs
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+
+namespace Grubs;
+
+public partial class GrubsGame : Game
 {
-	public partial class Game : Sandbox.Game
+	public static new GrubsGame Current => Game.Current as GrubsGame;
+
+	public GrubsGame()
 	{
-		public static Game Instance => Current as Game;
-		[Net] public StateHandler StateHandler { get; private set; }
 
-		public Game()
-		{
-			Terrain.Terrain.Initialize();
-			Quadtree.Initialize();
-			AssetPrecache.DoPrecache();
+	}
 
-			if ( IsServer )
-				StateHandler = new();
-		}
-
-		public override void ClientJoined( Client cl )
-		{
-			base.ClientJoined( cl );
-
-			var player = new Pawn.Player();
-			player.Respawn();
-			cl.Pawn = player;
-
-			StateHandler.OnPlayerJoin( player );
-		}
-
-		/// <summary>
-		/// Temporarily allow worms to noclip.
-		/// </summary>
-		public override void DoPlayerNoclip( Client player )
-		{
-			if ( !player.HasPermission( "noclip" ) )
-				return;
-
-			if ( player.Pawn is Pawn.Player basePlayer )
-			{
-				if ( basePlayer.ActiveWorm?.DevController is WormNoclipController )
-				{
-					Log.Info( "Noclip Mode Off" );
-					basePlayer.ActiveWorm.DevController = null;
-				}
-				else
-				{
-					Log.Info( "Noclip Mode On" );
-					basePlayer.ActiveWorm.DevController = new WormNoclipController();
-				}
-			}
-		}
-
-		public override void Simulate( Client cl )
-		{
-			base.Simulate( cl );
-
-			Quadtree.Simulate( cl );
-		}
+	public override void ClientJoined( Client cl )
+	{
+		base.ClientJoined( cl );
 	}
 }
