968270655fab7ff44b441a31cd3df8e84ee38b93
trundler-dev <57276947+trundler-dev@users.noreply.github.com>
2022-08-06 10:32:22 -0300

    Add AimReticle back in, adjust it's position

---
+++ code/Weapons/AimReticle.cs
@@ -0,0 +1,53 @@
+﻿namespace Grubs.Weapons;
+
+[Category( "Weapons" )]
+public partial class AimReticle : RenderEntity
+{
+	public Material Material = Material.Load( "materials/reticle/reticle.vmat" );
+
+	public Vector3 Direction = Vector3.Zero;
+	public float Power = 0.0f;
+
+	protected void DrawReticle( SceneObject obj, Vector3 startPos, Vector3 endPos, Vector3 direction, Vector3 size, Color color )
+	{
+		// vbos are drawn relative to world position
+		startPos -= Position;
+		endPos -= Position;
+
+		var vertexBuffer = Render.GetDynamicVB( true );
+
+		// Line
+		Vertex a = new( startPos - size, Vector3.Up, Vector3.Right, new Vector4( 0, 1, 0, 0 ) );
+		Vertex b = new( startPos + size, Vector3.Up, Vector3.Right, new Vector4( 1, 1, 0, 0 ) );
+		Vertex c = new( endPos + size, Vector3.Up, Vector3.Right, new Vector4( 1, 0, 0, 0 ) );
+		Vertex d = new( endPos - size, Vector3.Up, Vector3.Right, new Vector4( 0, 0, 0, 0 ) );
+
+		vertexBuffer.Add( a );
+		vertexBuffer.Add( b );
+		vertexBuffer.Add( c );
+		vertexBuffer.Add( d );
+
+		vertexBuffer.AddTriangleIndex( 4, 3, 2 );
+		vertexBuffer.AddTriangleIndex( 2, 1, 4 );
+
+		Render.Attributes.Set( "color", color );
+
+		vertexBuffer.Draw( Material );
+	}
+
+	public override void DoRender( SceneObject obj )
+	{
+		Render.SetupLighting( obj );
+
+		// Previously we were rendering in front of terrain.
+		// Setting to 0f for now.
+		var yOffset = 0f;
+
+		var startPos = Position.WithY( yOffset );
+		var endPos = (Position + (Direction * 30)).WithY( yOffset );
+		var size = Vector3.Cross( Direction, Vector3.Right ) * 15f;
+
+		var color = Color.White;
+		DrawReticle( obj, startPos, endPos, Direction, size, color );
+	}
+}
--- code/Weapons/GrubsWeapon.cs
+++ code/Weapons/GrubsWeapon.cs
@@ -9,6 +9,9 @@ public abstract partial class GrubsWeapon : BaseCarriable
 	public virtual string ModelPath => "";
 	public virtual int MaxFireCount => 1;
 	public virtual HoldPose HoldPose => HoldPose.None;
+	public virtual bool HasReticle => true;
+
+	public static AimReticle AimReticle;
 
 	[Net, Local] public int Ammo { get; set; }
 	[Net] public bool WeaponHasHat { get; set; }
@@ -50,12 +53,37 @@ public abstract partial class GrubsWeapon : BaseCarriable
 	public override void Simulate( Client cl )
 	{
 		base.Simulate( cl );
+
+		if ( IsClient && HasReticle )
+			AdjustReticle();
+	}
+
+	protected void AdjustReticle()
+	{
+		if ( !AimReticle.IsValid() )
+			AimReticle = new();
+
+		var upOffset = Vector3.Up * 5f;
+
+		var pos = Position + Parent.EyeRotation.Forward.Normal * 50;
+		var dir = Parent.EyeRotation.Forward.Normal;
+
+		var tr = Trace.Ray( Position, pos ).Size( AimReticle.RenderBounds ).WorldOnly().Run();
+
+		if ( tr.Hit )
+			pos = tr.EndPosition;
+
+		pos += upOffset;
+		AimReticle.Position = pos;
+		AimReticle.Direction = dir;
 	}
 
 	public void ShowWeapon( Worm worm, bool show )
 	{
 		EnableDrawing = show;
 		ShowHoldPose( show );
+
+		// TODO: How should we hide the AimReticle?
 
 		if ( WeaponHasHat )
 			worm.SetHatVisible( !show );
