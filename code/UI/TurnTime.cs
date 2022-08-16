e712c6d2ea6473f361deb3e7c14d0cc4d13021d2
Peter <petergorman11@gmail.com>
2022-08-16 18:50:07 -0400

    Add placeholder UI for waiting and play states

---
+++ code/UI/TurnTime.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using Grubs.States;
+using Grubs.Utils;
+
+namespace Grubs.UI;
+
+public class TurnTime : Panel
+{
+	private static PlayState State => GrubsGame.Current.CurrentState as PlayState;
+
+	private readonly Label _timeLeft;
+
+	public TurnTime()
+	{
+		StyleSheet.Load( "/UI/Stylesheets/TurnTime.scss" );
+
+		_timeLeft = Add.Label( GameConfig.TurnDuration.ToString(), "time-left" );
+	}
+
+	public override void Tick()
+	{
+		base.Tick();
+
+		if ( State is null )
+			return;
+
+		_timeLeft.Text = Math.Floor( State.TimeUntilTurnEnd ).ToString( CultureInfo.CurrentCulture );
+		
+		// TODO: Event for when turn changes and update there
+		foreach ( var teamName in GameConfig.TeamNames )
+			SetClass( $"team-{teamName}", GameConfig.TeamNames[State.TeamsTurn-1] == teamName );
+	}
+}
---
+++ code/UI/WaitingStatus.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+using Grubs.States;
+using Grubs.Utils;
+
+namespace Grubs.UI;
+
+public class WaitingStatus : Panel
+{
+	private static WaitingState State => GrubsGame.Current.CurrentState as WaitingState;
+
+	private readonly Label _message;
+	
+	public WaitingStatus()
+	{
+		StyleSheet.Load( "/UI/Stylesheets/WaitingStatus.scss" );
+		
+		_message = Add.Label( "???", "message" );
+	}
+
+	public override void Tick()
+	{
+		base.Tick();
+
+		if ( State is null )
+			return;
+
+		if ( !State.IsStarting )
+		{
+			_message.Text = $"Waiting for players ({Client.All.Count}/{GameConfig.MaximumPlayers})";
+			return;
+		}
+
+		var secondsTillStart = Math.Ceiling( State.TimeUntilStart ).ToString( CultureInfo.CurrentCulture );
+		_message.Text = $"Starting in {secondsTillStart} seconds ({Client.All.Count}/{GameConfig.MaximumPlayers})";
+	}
+}
