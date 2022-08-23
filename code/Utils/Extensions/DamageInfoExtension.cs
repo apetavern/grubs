891e7bfa9d9ab79e847e2f830454448a54f17d35
Peter <petergorman11@gmail.com>
2022-08-23 16:38:47 -0400

    Add Fortwars DamageInfoExtension

---
+++ code/Utils/Extensions/DamageInfoExtension.cs
@@ -0,0 +1,26 @@
+﻿namespace Grubs.Utils.Extensions;
+
+public class DamageInfoExtension
+{
+	public static DamageInfo FromFall( float damage, Entity attacker )
+	{
+		return new DamageInfo
+		{
+			Flags = DamageFlags.Fall,
+			Damage = damage,
+			Attacker = attacker
+		};
+	}
+
+	public static DamageInfo FromProjectile( float damage, Vector3 position, Vector3 force, Entity attacker )
+	{
+		return new DamageInfo
+		{
+			Flags = DamageFlags.Blast,
+			Position = position,
+			Damage = damage,
+			Attacker = attacker,
+			Force = force,
+		};
+	}
+}
