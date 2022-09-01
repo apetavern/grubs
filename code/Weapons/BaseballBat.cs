5611572c16b93f22b20c9c98fecd4d9999f38437
Peter <petergorman11@gmail.com>
2022-08-31 21:33:09 -0400

    Make baseball bat usable

--- code/Weapons/BaseballBat.cs
+++ code/Weapons/BaseballBat.cs
@@ -5,9 +5,12 @@ namespace Grubs.Weapons;
 /// <summary>
 /// A simple baseball bat.
 /// </summary>
-public class BaseballBat : GrubWeapon
+public class BaseballBat : MeleeWeapon
 {
 	protected override string WeaponName => "Baseball Bat";
 	protected override string ModelPath => "models/weapons/baseballbat/baseballbat.vmdl";
 	protected override HoldPose HoldPose => HoldPose.Swing;
+
+	protected override Vector3 HitSize => new( 50, 32, 50 );
+	protected override float Damage => 25;
 }
