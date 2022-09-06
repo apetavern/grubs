33878051736e32152c6c93e98aa0436d341728f0
Peter <petergorman11@gmail.com>
2022-09-06 11:44:39 -0400

    Check state before updating TurnTime UI

--- code/UI/TurnTime.cs
+++ code/UI/TurnTime.cs
@@ -1,5 +1,6 @@
 ﻿using System.Globalization;
 using Grubs.Player;
+using Grubs.States;
 using Grubs.Utils;
 
 namespace Grubs.UI;
@@ -13,12 +14,18 @@ public class TurnTime : Panel
 		StyleSheet.Load( "/UI/Stylesheets/TurnTime.scss" );
 
 		_timeLeft = Add.Label( GameConfig.TurnDuration.ToString(), "time-left" );
-		BindClass( "hidden", () => GrubsGame.Current.CurrentGamemode.UsedTurn );
 	}
 
 	public override void Tick()
 	{
 		base.Tick();
+
+		if ( GrubsGame.Current.CurrentState is not BaseGamemode )
+			return;
+
+		SetClass( "hidden", GrubsGame.Current.CurrentGamemode.UsedTurn );
+		if ( HasClass( "hidden" ) )
+			return;
 
 		_timeLeft.Text = Math.Floor( GrubsGame.Current.CurrentGamemode.TimeUntilTurnEnd ).ToString( CultureInfo.CurrentCulture );
 
