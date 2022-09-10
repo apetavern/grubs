87f9c881bc53cb6f5c20328563f32c7e7745964c
Brian Lindenhof <brian@shadowbrain.net>
2022-09-11 00:50:27 +0200

    Correct the height of the worm on inclines and smooth out the transition between angles a bit

--- code/Player/Grub/GrubAnimator.cs
+++ code/Player/Grub/GrubAnimator.cs
@@ -4,6 +4,13 @@ namespace Grubs.Player;
 
 public class GrubAnimator : PawnAnimator
 {
+	enum sidehigher
+	{
+		front,
+		back
+	}
+
+	float incline;
 	public override void Simulate()
 	{
 		var grub = Pawn as Grub;
@@ -15,14 +22,18 @@ public class GrubAnimator : PawnAnimator
 		SetAnimParameter( "explode", grub.LifeState == LifeState.Dying );
 		SetAnimParameter( "sliding", grub.HasBeenDamaged && !controller.IsHardFalling && !controller.Velocity.IsNearlyZero( 2.5f ) );
 
-		float velocity = Pawn.Velocity.Cross( Vector3.Up ).Length;
+		float velocity = WishVelocity.Length;// Pawn.Velocity.Cross( Vector3.Up ).Length;
 		SetAnimParameter( "velocity", velocity );
 
 		float aimAngle = -Pawn.EyeRotation.Pitch().Clamp( -80f, 75f );
 		SetAnimParameter( "aimangle", aimAngle );
 
-		var tr = Trace.Ray( Pawn.Position, Pawn.Position + Pawn.Rotation.Down * 128 ).Ignore( Pawn ).IncludeClientside().Run();
-		float incline = Pawn.Rotation.Forward.Angle( tr.Normal ) - 90f;
+		var tr = Trace.Ray( Pawn.Position + Pawn.Rotation.Up * 10f, Pawn.Position + Pawn.Rotation.Down * 128 ).Ignore( Pawn ).IncludeClientside().Run();
+
+		incline = MathX.Lerp( incline, Pawn.Rotation.Forward.Angle( tr.Normal ) - 90f, 0.25f );
+
 		SetAnimParameter( "incline", incline );
+
+		SetAnimParameter( "heightdiff", tr.Distance );
 	}
 }
