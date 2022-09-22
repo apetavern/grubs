78a007236e60228e02d21d67cf3cc140a46fbf94
Peter <petergorman11@gmail.com>
2022-09-22 18:37:48 -0400

    Step PremadeTerrain version and add conversion support for V1

--- code/Terrain/PremadeTerrain/PremadeTerrain.cs
+++ code/Terrain/PremadeTerrain/PremadeTerrain.cs
@@ -12,7 +12,7 @@ public sealed class PremadeTerrain
 	/// <summary>
 	/// The current version of pre-made terrains. Used for serializing/deserializing.
 	/// </summary>
-	public const int Version = 1;
+	public const int Version = 2;
 
 	/// <summary>
 	/// The version of the terrain that was loaded.
@@ -29,7 +29,7 @@ public sealed class PremadeTerrain
 	/// <summary>
 	/// The grid array of the terrain.
 	/// </summary>
-	public readonly bool[,] TerrainGrid;
+	public readonly bool[] TerrainGrid;
 	/// <summary>
 	/// The settings that came with the terrain.
 	/// </summary>
@@ -77,7 +77,7 @@ public sealed class PremadeTerrain
 		}
 	}
 
-	private PremadeTerrain( int version, int width, int height, bool[,] terrainGrid,
+	private PremadeTerrain( int version, int width, int height, bool[] terrainGrid,
 		Dictionary<TerrainSetting, object> settings )
 	{
 		MapVersion = version;
@@ -101,6 +101,124 @@ public sealed class PremadeTerrain
 		if ( version != Version )
 			return ConvertTerrain( version, reader );
 
+		// Read settings.
+		var numSettings = reader.ReadInt32();
+		var settings = new Dictionary<TerrainSetting, object>( numSettings );
+		for ( var i = 0; i < numSettings; i++ )
+		{
+			switch ( (TerrainSetting)reader.ReadByte() )
+			{
+				case TerrainSetting.Border:
+					settings.Add( TerrainSetting.Border, reader.ReadBoolean() );
+					break;
+				case TerrainSetting.Scale:
+					settings.Add( TerrainSetting.Scale, reader.ReadInt32() );
+					break;
+				case TerrainSetting.TerrainType:
+					var terrainTypeStr = reader.ReadString();
+					if ( !Enum.TryParse( terrainTypeStr, out TerrainType terrainType ) )
+					{
+						Log.Error( $"Got invalid terrain type \"{terrainTypeStr}\", reverting to {GameConfig.TerrainType}" );
+						terrainType = GameConfig.TerrainType;
+					}
+
+					settings.Add( TerrainSetting.TerrainType, terrainType );
+					break;
+				default:
+					throw new ArgumentOutOfRangeException( nameof( TerrainSetting ) );
+			}
+		}
+
+		// Read basic terrain data.
+		var width = reader.ReadInt32();
+		var height = reader.ReadInt32();
+		var terrainGrid = new bool[width * height];
+		var currentValue = reader.ReadBoolean();
+		terrainGrid[0] = currentValue;
+
+		// Read changes in terrain.
+		var numChanges = reader.ReadInt32();
+		var currentCoord = 0;
+		for ( var i = 0; i < numChanges; i++ )
+		{
+			var coord = reader.ReadInt32();
+			while ( currentCoord < coord )
+			{
+				terrainGrid[currentCoord] = currentValue;
+				currentCoord++;
+			}
+
+			currentValue = !currentValue;
+		}
+
+		return new PremadeTerrain( 2, width, height, terrainGrid, settings );
+	}
+
+	/// <summary>
+	/// Writes a terrains data.
+	/// </summary>
+	/// <param name="writer">The <see cref="BinaryWriter"/> to write the terrain data to.</param>
+	/// <param name="map">The map whose data to write.</param>
+	/// <param name="preserveSettings">Whether or not to save the <see cref="TerrainSetting"/>s alongside the terrain.</param>
+	public static void Serialize( BinaryWriter writer, TerrainMap map, bool preserveSettings )
+	{
+		// Write version
+		writer.Write( Version );
+		// Write settings.
+		if ( preserveSettings )
+		{
+			// Write last pre-made terrains settings.
+			if ( map.Premade )
+			{
+				writer.Write( map.PremadeMap!.Settings.Count );
+				foreach ( var pair in map.PremadeMap.Settings )
+					writer.Write( pair.Key, pair.Value );
+			}
+			// Write random gen settings.
+			else
+			{
+				var values = Enum.GetValues<TerrainSetting>();
+				writer.Write( values.Length );
+				foreach ( var setting in values )
+					writer.Write( setting );
+			}
+		}
+		// Don't write any settings.
+		else
+			writer.Write( 0 );
+
+		// Fetch all terrain changes.
+		var currentValue = map.TerrainGrid[0];
+		var valueChanges = new List<int>();
+		for ( var i = 0; i < map.TerrainGrid.Length; i++ )
+		{
+			if ( currentValue == map.TerrainGrid[i] )
+				continue;
+
+			valueChanges.Add( i );
+			currentValue = !currentValue;
+		}
+
+		// Write terrain data.
+		writer.Write( map.Width );
+		writer.Write( map.Height );
+		writer.Write( map.TerrainGrid[0] );
+		writer.Write( valueChanges.Count );
+		foreach ( var index in valueChanges )
+			writer.Write( index );
+	}
+
+	private static PremadeTerrain ConvertTerrain( int mapVersion, BinaryReader reader )
+	{
+		return mapVersion switch
+		{
+			1 => ConvertV1( reader ),
+			_ => throw new ArgumentException( nameof( mapVersion ) )
+		};
+	}
+
+	private static PremadeTerrain ConvertV1( BinaryReader reader )
+	{
 		// Read settings.
 		var numSettings = reader.ReadInt32();
 		var settings = new Dictionary<TerrainSetting, object>( numSettings );
@@ -161,71 +279,12 @@ public sealed class PremadeTerrain
 			currentValue = !currentValue;
 		}
 
-		return new PremadeTerrain( version, width, height, terrainGrid, settings );
-	}
+		// Convert 2D terrain grid to 1D index array.
+		var correctTerrainGrid = new bool[width * height];
+		for ( var y = 0; y < height; y++ )
+			for ( var x = 0; x < width; x++ )
+				correctTerrainGrid[Dimensions.Convert2dTo1d( x, y, width )] = terrainGrid[x, y];
 
-	/// <summary>
-	/// Writes a terrains data.
-	/// </summary>
-	/// <param name="writer">The <see cref="BinaryWriter"/> to write the terrain data to.</param>
-	/// <param name="map">The map whose data to write.</param>
-	/// <param name="preserveSettings">Whether or not to save the <see cref="TerrainSetting"/>s alongside the terrain.</param>
-	public static void Serialize( BinaryWriter writer, TerrainMap map, bool preserveSettings )
-	{
-		// Write version
-		writer.Write( Version );
-		// Write settings.
-		if ( preserveSettings )
-		{
-			// Write last pre-made terrains settings.
-			if ( map.Premade )
-			{
-				writer.Write( map.PremadeMap!.Settings.Count );
-				foreach ( var pair in map.PremadeMap.Settings )
-					writer.Write( pair.Key, pair.Value );
-			}
-			// Write random gen settings.
-			else
-			{
-				var values = Enum.GetValues<TerrainSetting>();
-				writer.Write( values.Length );
-				foreach ( var setting in values )
-					writer.Write( setting );
-			}
-		}
-		// Don't write any settings.
-		else
-			writer.Write( 0 );
-
-		// Fetch all terrain changes.
-		var currentValue = map.TerrainGrid[0, 0];
-		var valueChanges = new List<(int, int)>();
-		for ( var y = 0; y < map.TerrainGrid.GetLength( 1 ); y++ )
-		{
-			for ( var x = 0; x < map.TerrainGrid.GetLength( 0 ); x++ )
-			{
-				if ( currentValue == map.TerrainGrid[x, y] )
-					continue;
-
-				valueChanges.Add( (x, y) );
-				currentValue = !currentValue;
-			}
-		}
-
-		// Write terrain data.
-		writer.Write( map.Width );
-		writer.Write( map.Height );
-		writer.Write( currentValue );
-		writer.Write( valueChanges.Count );
-		for ( var i = 0; i < valueChanges.Count; i++ )
-		{
-			writer.Write( valueChanges[i].Item1 );
-			writer.Write( valueChanges[i].Item2 );
-		}
-	}
-
-	private static PremadeTerrain ConvertTerrain( int mapVersion, BinaryReader reader )
-	{
-		return null!;
+		return new PremadeTerrain( Version, width, height, correctTerrainGrid, settings );
 	}
 }
