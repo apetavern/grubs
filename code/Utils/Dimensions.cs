cf68ea54c93778ed9c0341093024b92731f6eb92
Peter <petergorman11@gmail.com>
2022-09-22 18:35:09 -0400

    Add dimensions utility class

---
+++ code/Utils/Dimensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Grubs.Utils;
+
+public static class Dimensions
+{
+	public static int Convert2dTo1d( int x, int y, int width )
+	{
+		return x + width * y;
+	}
+
+	public static (int, int) Convert1dTo2d( int index, int width )
+	{
+		return (index % width, index / width);
+	}
+}
