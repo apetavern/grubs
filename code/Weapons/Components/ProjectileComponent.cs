898272bdd5d8dd55b832407d43c2aefb3cd6393a
Brian Lindenhof <brian@shadowbrain.net>
2023-02-28 19:47:25 +0100

    Implement FireCursor on projectile component and make it drop the projectile from the sky where you clicked

--- code/Weapons/Components/ProjectileComponent.cs
+++ code/Weapons/Components/ProjectileComponent.cs
@@ -10,7 +10,13 @@ public partial class ProjectileComponent : WeaponComponent
 	public bool ProjectileShouldBounce { get; set; } = false;
 
 	[Prefab]
+	public bool ProjectileShouldRotate { get; set; } = true;
+
+	[Prefab]
 	public bool ProjectileShouldUseTrace { get; set; } = false;
+
+	[Prefab]
+	public bool ProjectileShouldUseModelCollision { get; set; } = false;
 
 	[Prefab]
 	public int ProjectileMaxBounces { get; set; } = 0;
@@ -44,6 +50,60 @@ public partial class ProjectileComponent : WeaponComponent
 
 		if ( IsFiring )
 			Fire();
+	}
+
+	public override void FireCursor()
+	{
+		Log.Info( "Fire Cursor" );
+
+		if ( !Game.IsServer )
+			return;
+
+		var position = Grub.Player.MousePosition.WithZ( 1000 );
+
+		var projectile = new Projectile()
+			.WithGrub( Grub )
+			.WithModel( ProjectileModel )
+			.WithPosition( position )
+			.WithSpeed( ProjectileSpeed )
+			.WithExplosionSound( ProjectileExplosionSound )
+			.WithExplosionRadius( ProjectileExplosionRadius )
+			.WithBouncing( ProjectileShouldBounce )
+			.WithRotate( ProjectileShouldRotate )
+			.SetCollisionReaction( ProjectileCollisionReaction.Explosive );
+
+		if ( ProjectileShouldUseTrace )
+		{
+			var arcTrace = new ArcTrace( Grub, Grub.Player.MousePosition.WithZ( 1000 ) );
+			var segments = ProjectileShouldBounce
+				? arcTrace.RunTowardsWithBounces( -Vector3.Up, ProjectileForceMultiplier * Charge, 0, ProjectileMaxBounces )
+				: arcTrace.RunTowards( -Vector3.Up, ProjectileForceMultiplier * Charge, 0f );
+
+			projectile.MoveAlongTrace( segments );
+		}
+		else
+		{
+			if ( ProjectileShouldUseModelCollision )
+			{
+				projectile.SetupPhysicsFromModel( PhysicsMotionType.Keyframed );
+			}
+			else
+			{
+				projectile.SetupPhysicsFromSphere( PhysicsMotionType.Keyframed, position, ProjectileRadius );
+			}
+			var desiredPosition = position - Vector3.Up * 40f;
+			var tr = Trace.Ray( position, desiredPosition ).Ignore( projectile ).Ignore( Weapon.Owner ).Run();
+			projectile.Position = tr.EndPosition;
+			projectile.Velocity = (-Vector3.Up * ProjectileSpeed).WithY( 0f );
+		}
+
+		if ( ProjectileExplodeAfter > 0 )
+			projectile.ExplodeAfterSeconds( ProjectileExplodeAfter );
+
+		projectile.Finish();
+		Grub.SetAnimParameter( "fire", true );
+		IsFiring = false;
+		FireFinished();
 	}
 
 	public override void FireInstant()
