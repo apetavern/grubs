99db6ed43d9cc5c293717bfa56cbd78eb0ef5576
Matthew Zegar <matthewzegar@gmail.com>
2023-03-02 22:39:27 -0800

    Add back dynamite

---
+++ code/Weapons/Explosives/Components/DroppableComponent.cs
@@ -0,0 +1,15 @@
+namespace Grubs;
+
+[Prefab]
+public partial class DroppableComponent : ExplosiveComponent
+{
+	public override void OnFired( Weapon weapon, int charge )
+	{
+		base.OnFired( weapon, charge );
+
+		var desiredPosition = Grub.Position + (Grub.EyeRotation.Forward.Normal * Grub.Facing * 40f);
+		var tr = Trace.Ray( desiredPosition, desiredPosition ).Ignore( Grub ).Run(); // This trace is incorrect, should be from position -> desired position.
+		Explosive.Position = tr.EndPosition;
+		Explosive.Velocity = (Grub.EyeRotation.Forward.Normal * Grub.Facing).WithY( 0f );
+	}
+}
