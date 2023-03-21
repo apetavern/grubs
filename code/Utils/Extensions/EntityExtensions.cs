9653330239128e927390ab583eb8941fcf9a2a58
Matthew Zegar <matthewzegar@gmail.com>
2023-03-21 11:29:14 -0700

    Add dynamite fuse sound

--- code/Utils/Extensions/EntityExtensions.cs
+++ code/Utils/Extensions/EntityExtensions.cs
@@ -2,14 +2,11 @@
 
 public static class EntityExtensions
 {
-	public static void SoundFromScreen( this Entity e, string sound )
+	public static Sound SoundFromScreen( this Entity e, string sound )
 	{
 		Assert.True( Game.IsClient );
 
-		if ( !e.IsValid() )
-			return;
-
 		var pos = e.Position.ToScreen();
-		Sound.FromScreen( sound, pos.x, pos.y );
+		return Sound.FromScreen( sound, pos.x, pos.y );
 	}
 }
