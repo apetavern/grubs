6d72874ee995b242281fe31efe5f8728274aec03
Matthew Zegar <32153021+matekdev@users.noreply.github.com>
2023-03-21 19:19:16 -0700

    Add bazooka sound and particle (#210)

--- code/Weapons/Components/GadgetWeaponComponent.cs
+++ code/Weapons/Components/GadgetWeaponComponent.cs
@@ -6,6 +6,12 @@ public partial class GadgetWeaponComponent : WeaponComponent
 	[Prefab, Net]
 	public Prefab GadgetPrefab { get; set; }
 
+	[Prefab, Net, ResourceType( "vpcf" )]
+	public string MuzzleParticle { get; set; }
+
+	[Prefab, ResourceType( "sound" )]
+	public string UseSound { get; set; }
+
 	public override void Simulate( IClient client )
 	{
 		base.Simulate( client );
@@ -28,11 +34,22 @@ public partial class GadgetWeaponComponent : WeaponComponent
 	{
 		Grub.SetAnimParameter( "fire", true );
 
+		if ( !string.IsNullOrEmpty( MuzzleParticle ) )
+		{
+			var muzzle = Weapon.GetAttachment( "muzzle" );
+			if ( muzzle is not null )
+			{
+				var muzzleFlash = Particles.Create( MuzzleParticle, muzzle.Value.Position );
+				muzzleFlash.SetOrientation( 0, muzzle.Value.Rotation.Angles() );
+			}
+		}
+
 		if ( !Game.IsServer )
 			return;
 
 		var gadget = PrefabLibrary.Spawn<Gadget>( GadgetPrefab );
 		gadget.OnUse( Grub, Weapon, Charge );
+		gadget.PlayScreenSound( UseSound );
 
 		IsFiring = false;
 		Charge = MinCharge;
