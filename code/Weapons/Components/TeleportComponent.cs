b478d566a51dea615b85f790d5ec2c7caf41b926
Matthew Zegar <matthewzegar@gmail.com>
2023-03-21 19:35:18 -0700

    Teleport uses a grub as a preview

--- code/Weapons/Components/TeleportComponent.cs
+++ code/Weapons/Components/TeleportComponent.cs
@@ -4,14 +4,52 @@
 public partial class TeleportComponent : WeaponComponent
 {
 	[Net]
-	public Vector2 MousePosition { get; set; }
+	public AnimatedEntity TeleportPreview { get; set; }
+
+	public override void OnDeploy()
+	{
+		if ( !Game.IsServer )
+			return;
+
+		TeleportPreview = new AnimatedEntity( "models/citizenworm.vmdl" );
+		TeleportPreview.SetupPhysicsFromModel( PhysicsMotionType.Static );
+		TeleportPreview.SetAnimParameter( "grounded", true );
+		TeleportPreview.Tags.Add( "trigger" );
+		TeleportPreview.Owner = Grub;
+	}
+
+	public override void OnHolster()
+	{
+		base.OnHolster();
+
+		if ( Game.IsServer && TeleportPreview.IsValid() )
+			TeleportPreview.Delete();
+	}
 
 	public override void Simulate( IClient client )
 	{
 		base.Simulate( client );
 
-		if ( IsFiring )
+		if ( !TeleportPreview.IsValid() )
+			return;
+
+		TeleportPreview.EnableDrawing = Grub.Controller.ShouldShowWeapon();
+
+		if ( !TeleportPreview.EnableDrawing )
+			return;
+
+		TeleportPreview.Position = Grub.Player.MousePosition;
+
+		var isValidPlacement = !Trace.Box( Grub.Controller.Hull, Grub.Player.MousePosition, Grub.Player.MousePosition ).Ignore( TeleportPreview ).Run().Hit;
+		TeleportPreview.RenderColor = isValidPlacement ? Color.Green : Color.Red;
+
+		if ( !IsFiring )
+			return;
+
+		if ( isValidPlacement )
 			Fire();
+
+		IsFiring = false;
 	}
 
 	public override void FireCursor()
@@ -20,12 +58,5 @@ public partial class TeleportComponent : WeaponComponent
 		Grub.Position = Grub.Player.MousePosition;
 
 		FireFinished();
-	}
-
-	public override void FireFinished()
-	{
-		base.FireFinished();
-
-		Event.Run( GrubsEvent.Player.PointerEventChanged, false );
 	}
 }
