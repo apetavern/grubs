269a6188de070aecc581c2a61d3183a3be13632d
Matthew Zegar <matthewzegar@gmail.com>
2023-04-22 11:32:33 -0700

    Stylize start button

--- code/UI/MainMenu/PlayMenu.cs
+++ code/UI/MainMenu/PlayMenu.cs
@@ -5,12 +5,6 @@ public partial class PlayMenu : Panel
 	[ConCmd.Server]
 	public static void StartGame()
 	{
-		if ( Game.Clients.Count < GamemodeSystem.Instance.MinimumPlayers )
-			return;
-
-		if ( !GamemodeSystem.Instance.WorldReady || GamemodeSystem.Instance.CurrentState != Gamemode.State.MainMenu )
-			return;
-
 		GamemodeSystem.Instance.Start();
 	}
 
