7dfbc66c4340f68a051d8d8ae228e8ade3e21291
Matthew Zegar <matthewzegar@gmail.com>
2023-05-04 18:15:24 -0700

    Revert camera clamp for now

--- code/Player/GrubsCamera.cs
+++ code/Player/GrubsCamera.cs
@@ -44,7 +44,6 @@ public class GrubsCamera
 		var targetPosition = cameraCenter + Vector3.Right * Distance;
 		var currentPosition = Camera.Position;
 		Camera.Position = currentPosition.LerpTo( targetPosition, Time.Delta * Math.Max( _lerpSpeed, _target.Velocity.Length / 50 ) );
-		Camera.Position = Camera.Position.Clamp( World.WorldMins.WithZ( 16f ), World.WorldMaxs );
 
 		var lookDir = (cameraCenter - targetPosition).Normal;
 		Camera.Rotation = Rotation.LookAt( lookDir, Vector3.Up );
