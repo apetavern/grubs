3b261aef1fe4b331a9e3ec1191cc1f60dcee2ffe
Brian Lindenhof <brian@shadowbrain.net>
2023-05-13 00:18:09 +0200

    Initial re-add of terrain textures- currently only Cavern works!

--- code/Terrain/Terrain.Texture.cs
+++ code/Terrain/Terrain.Texture.cs
@@ -1,9 +1,25 @@
-﻿namespace Grubs;
+﻿using Sandbox.Sdf;
+
+namespace Grubs;
 
 public partial class Terrain
 {
 	void SetupWorldFromTexture()
 	{
+		DoTextureLoad();
+	}
+
+	async void DoTextureLoad()
+	{
+		var mapSdfTexture = await Texture.LoadAsync( FileSystem.Mounted, "textures/texturelevels/" + GrubsConfig.WorldTerrainTexture.ToString() + ".png" );
+		var mapSdf = new TextureSdf( mapSdfTexture, 10, mapSdfTexture.Width * 2f );
+
+		SdfWorld.Add( mapSdf, SandMaterial );
+
+		mapSdfTexture = await Texture.LoadAsync( FileSystem.Mounted, "textures/texturelevels/" + GrubsConfig.WorldTerrainTexture.ToString() + "_back.png" );
+		mapSdf = new TextureSdf( mapSdfTexture, 10, mapSdfTexture.Width * 2f );
+
+		SdfWorld.Add( mapSdf, RockMaterial );
 
 	}
 }
