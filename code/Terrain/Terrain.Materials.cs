6a5723c0af403daadc5b8b7d503231a5c643fadd
trundler-dev <57276947+trundler-dev@users.noreply.github.com>
2023-05-12 15:56:25 -0300

    Allow GetSandMaterials to take in optional bg and fg offsets

--- code/Terrain/Terrain.Materials.cs
+++ code/Terrain/Terrain.Materials.cs
@@ -8,13 +8,17 @@ public partial class Terrain
 	public Sdf2DMaterial SandMaterial { get; } = ResourceLibrary.Get<Sdf2DMaterial>( "materials/sdf/sand.sdflayer" );
 	public Sdf2DMaterial RockMaterial { get; } = ResourceLibrary.Get<Sdf2DMaterial>( "materials/sdf/rock.sdflayer" );
 
-	private Dictionary<Sdf2DMaterial, float> GetSandMaterials( bool includeForeground = true, bool includeBackground = false )
+	private Dictionary<Sdf2DMaterial, float> GetSandMaterials(
+		bool includeForeground = true,
+		bool includeBackground = false,
+		float fgOffset = 0,
+		float bgOffset = 0 )
 	{
 		var sandMaterials = new Dictionary<Sdf2DMaterial, float>();
 		if ( includeForeground )
-			sandMaterials.Add( SandMaterial, 0f );
+			sandMaterials.Add( SandMaterial, fgOffset );
 		if ( includeBackground )
-			sandMaterials.Add( RockMaterial, 0f );
+			sandMaterials.Add( RockMaterial, bgOffset );
 
 		return sandMaterials;
 	}
