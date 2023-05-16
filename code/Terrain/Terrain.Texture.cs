c4c523b28b5f38b45c4d5fb2bba9378a6a00e022
Brian Lindenhof <brian@shadowbrain.net>
2023-05-16 13:03:27 +0200

    Shift island texture spawning over a bit to fix left-sided spawning

--- code/Terrain/Terrain.Texture.cs
+++ code/Terrain/Terrain.Texture.cs
@@ -20,7 +20,7 @@ public partial class Terrain
 		GrubsConfig.TerrainHeight = mapSdfTexture.Height;
 
 		var mapSdf = new TextureSdf( mapSdfTexture, 10, mapSdfTexture.Width * 2f );
-		var transformedSdf = mapSdf.Transform( new Vector2( -GrubsConfig.TerrainLength / 2, 0 ) );
+		var transformedSdf = mapSdf.Transform( new Vector2( -GrubsConfig.TerrainLength, 0 ) );
 
 		var cfg = new MaterialsConfig( true, true );
 		var materials = GetActiveMaterials( cfg );
@@ -29,7 +29,7 @@ public partial class Terrain
 
 		mapSdfTexture = await Texture.LoadAsync( FileSystem.Mounted, "textures/texturelevels/" + GrubsConfig.WorldTerrainTexture.ToString() + "_back.png" );
 		mapSdf = new TextureSdf( mapSdfTexture, 10, mapSdfTexture.Width * 2f );
-		transformedSdf = mapSdf.Transform( new Vector2( -GrubsConfig.TerrainLength / 2, 0 ) );
+		transformedSdf = mapSdf.Transform( new Vector2( -GrubsConfig.TerrainLength, 0 ) );
 
 		SdfWorld.Add( transformedSdf, materials.ElementAt( 1 ).Key );
 
