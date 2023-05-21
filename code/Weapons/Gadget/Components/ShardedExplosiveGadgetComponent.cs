ef637a9874e7cfec592da624a6bcf94f7051329d
Brian Lindenhof <brian@shadowbrain.net>
2023-05-21 17:00:46 +0200

    Initial Cluster Grenade implementation

---
+++ code/Weapons/Gadget/Components/ShardedExplosiveGadgetComponent.cs
@@ -0,0 +1,46 @@
+namespace Grubs;
+
+[Prefab]
+public partial class ShardedExplosiveGadgetComponent : ExplosiveGadgetComponent
+{
+	[Prefab, Net]
+	public Prefab ShardGadgetPrefab { get; set; }
+
+	[Prefab]
+	public int AmountOfShards { get; set; }
+
+	[Prefab]
+	public int ShardCharge { get; set; } = 30;
+
+	public override bool IsResolved()
+	{
+		return TimeUntilExplosion;
+	}
+
+	public override void Explode()
+	{
+		if ( !Game.IsServer )
+			return;
+
+		switch ( ExplosionReaction )
+		{
+			case ExplosiveReaction.Explosion:
+				ExplosionHelper.Explode( Gadget.Position, Grub, ExplosionRadius, MaxExplosionDamage );
+				break;
+			case ExplosiveReaction.Incendiary:
+				FireHelper.StartFiresAt( Gadget.Position, Gadget.Velocity.Normal, 10 );
+				break;
+		}
+
+		for ( int i = 0; i < AmountOfShards; i++ )
+		{
+			var gadget = PrefabLibrary.Spawn<Gadget>( ShardGadgetPrefab );
+			var DirectionRNG = new Random( Time.Tick + i );
+			var direction = Rotation.LookAt( Vector3.Up + Vector3.Forward * DirectionRNG.Float() * 0.5f, Vector3.Right );
+			gadget.OnUse( Grub, Gadget.Position, direction, (int)MathF.Round( ShardCharge * DirectionRNG.Float( 0.5f, 1f ) ) );
+		}
+
+		ExplodeSoundClient( To.Everyone, ExplosionSound );
+		Gadget.Delete();
+	}
+}
