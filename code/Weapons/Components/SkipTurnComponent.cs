3dcd6d883d1cca486a5b77d5420da628e0fc6097
Matthew Zegar <matthewzegar@gmail.com>
2023-05-24 09:39:26 -0700

    Replace skip turn controller with smoke break

--- code/Weapons/Components/SkipTurnComponent.cs
+++ code/Weapons/Components/SkipTurnComponent.cs
@@ -3,6 +3,9 @@ namespace Grubs;
 [Prefab]
 public partial class SkipTurnComponent : WeaponComponent
 {
+	[Prefab, Net]
+	public ParticleSystem SmokeParticle { get; set; }
+
 	public override void Simulate( IClient client )
 	{
 		base.Simulate( client );
@@ -13,6 +16,14 @@ public partial class SkipTurnComponent : WeaponComponent
 
 	public override void FireInstant()
 	{
+		var muzzle = Weapon.GetAttachment( "muzzle" );
+
+		if ( Prediction.FirstTime && SmokeParticle is not null && muzzle is not null )
+		{
+			var muzzleFlash = Particles.Create( SmokeParticle.ResourcePath, muzzle.Value.Position );
+			muzzleFlash.SetOrientation( 0, muzzle.Value.Rotation.Angles() );
+		}
+
 		FireFinished();
 	}
 }
