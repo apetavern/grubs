9b96730715228aaa2ecfd3e2ad8637665531d91b
dch <>
2023-05-26 20:21:16 -0500

    better comments for the soundboard resource file

--- code/SoundBoard/SoundBoardSound.cs
+++ code/SoundBoard/SoundBoardSound.cs
@@ -5,12 +5,13 @@ public class SoundboardSound : GameResource
 {
 	/// <summary>
 	/// The name shown to players in the UI.
+	/// Also used as a parameter in the ServerCmd for playing sounds. 
 	/// </summary>
 	/// <value></value>
 	public string Title { get; set; }
 
 	/// <summary>
-	/// The Emoji to serve as an icon in the UI.
+	/// The Emoji to serve as an icon for the sound.
 	/// </summary>
 	/// <value></value>
 	public string Icon { get; set; }
