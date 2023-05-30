2db5aefab2e66628a1e7de1b3cc85827ce1cd7e9
Matthew Zegar <matthewzegar@gmail.com>
2023-05-29 22:50:27 -0700

    Rotate grub mid-air

--- code/Weapons/Components/JetpackComponent.cs
+++ code/Weapons/Components/JetpackComponent.cs
@@ -19,13 +19,13 @@ public partial class JetpackComponent : WeaponComponent
 		if ( !Prediction.FirstTime )
 			return;
 
-		_leftJetParticle = Particles.Create( "particles/blueflame/blueflame_continuous.vpcf" );
-		_rightJetParticle = Particles.Create( "particles/blueflame/blueflame_continuous.vpcf" );
-		_bottomJetParticle = Particles.Create( "particles/blueflame/blueflame_continuous.vpcf" );
+		// _leftJetParticle = Particles.Create( "particles/blueflame/blueflame_continuous.vpcf" );
+		// _rightJetParticle = Particles.Create( "particles/blueflame/blueflame_continuous.vpcf" );
+		// _bottomJetParticle = Particles.Create( "particles/blueflame/blueflame_continuous.vpcf" );
 
-		_leftJetParticle.SetEntityAttachment( 0, Weapon, "jet_middle" );
-		_rightJetParticle.SetEntityAttachment( 0, Weapon, "jet_left" );
-		_bottomJetParticle.SetEntityAttachment( 0, Weapon, "jet_right" );
+		// _leftJetParticle.SetEntityAttachment( 0, Weapon, "jet_middle" );
+		// _rightJetParticle.SetEntityAttachment( 0, Weapon, "jet_left" );
+		// _bottomJetParticle.SetEntityAttachment( 0, Weapon, "jet_right" );
 	}
 
 	public override void OnHolster()
@@ -34,9 +34,9 @@ public partial class JetpackComponent : WeaponComponent
 
 		Grub.SetAnimParameter( "jetpack_active", false );
 
-		_leftJetParticle?.Destroy( true );
-		_rightJetParticle?.Destroy( true );
-		_bottomJetParticle?.Destroy( true );
+		// _leftJetParticle?.Destroy( true );
+		// _rightJetParticle?.Destroy( true );
+		// _bottomJetParticle?.Destroy( true );
 	}
 
 	public override void Simulate( IClient client )
@@ -48,7 +48,6 @@ public partial class JetpackComponent : WeaponComponent
 		var verticalInput = -Grub.LookInput * Grub.Facing;
 
 		var isAscending = verticalInput > 0;
-		var isReversing = horizontalInput != Grub.Facing;
 
 		if ( !isAscending && controller.IsGrounded )
 			return;
@@ -56,15 +55,15 @@ public partial class JetpackComponent : WeaponComponent
 		Grub.Controller.ClearGroundEntity();
 		Grub.SetAnimParameter( "jetpack_dir", Grub.Facing * horizontalInput );
 
-		var horizontalVelocity = !isReversing ? MaxHorizontalSpeed * horizontalInput : MaxHorizontalSpeed * horizontalInput * 0.85f;
+		if ( Grub.MoveInput != 0 )
+			Grub.Rotation = Grub.MoveInput != 1 ? Rotation.Identity : Rotation.From( 0, 180, 0 );
+
+		var horizontalVelocity = horizontalInput * MaxHorizontalSpeed;
 		var verticalVelocity = verticalInput * MaxVerticalSpeed;
 
 		if ( verticalVelocity > 0 )
 			controller.Velocity = controller.Velocity.WithZ( verticalVelocity );
 
 		controller.Velocity = Vector3.Lerp( controller.Velocity, controller.Velocity.WithX( horizontalVelocity ), Time.Delta );
-
-		var mover = new MoveHelper( controller.Position, controller.Velocity );
-		mover.Trace = mover.Trace.Size( controller.Hull ).Ignore( Grub );
 	}
 }
