ac654d33b4e0eccf1a1dfb5b330cda01a772d7d8
Matthew Zegar <matthewzegar@gmail.com>
2023-05-31 11:56:06 -0700

    Add a glyph for remote detonate gadgets

--- code/Weapons/Gadget/Components/RemoteDetonateGadgetComponent.cs
+++ code/Weapons/Gadget/Components/RemoteDetonateGadgetComponent.cs
@@ -3,6 +3,11 @@ namespace Grubs;
 [Prefab]
 public partial class RemoteDetonateGadgetComponent : GadgetComponent
 {
+	public override void ClientSpawn()
+	{
+		_ = new UI.InputGlyphWorldPanel( Gadget, InputAction.Fire );
+	}
+
 	public override void Simulate( IClient client )
 	{
 		if ( Input.Down( InputAction.Fire ) )
