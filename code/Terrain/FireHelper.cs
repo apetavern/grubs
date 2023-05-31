41ad1257fc3b746d747feaf3e1ace6b95a0a64bc
Brian Lindenhof <brian@shadowbrain.net>
2023-05-31 13:45:47 +0200

    Add "EmitFiresAt" to the fire helper and give more control over the resulting fire particles

--- code/Terrain/FireHelper.cs
+++ code/Terrain/FireHelper.cs
@@ -14,6 +14,19 @@ public static class FireHelper
 				baseDirection * moveDirection );
 		}
 	}
+
+	public static void EmitFiresAt( Vector3 position, Vector3 moveDirection, int fireQuantity )
+	{
+		Game.AssertServer();
+
+		for ( var i = 0; i < fireQuantity * 5; i++ )
+		{
+			_ = new FireEntity(
+				position + moveDirection.Normal * Game.Random.Float(),
+				moveDirection * Game.Random.Float( 0.8f, 1f ),
+				Game.PhysicsWorld.Gravity * Time.Delta / 2f );
+		}
+	}
 }
 
 [Category( "Weapons" )]
@@ -27,6 +40,8 @@ public class FireEntity : ModelEntity, IResolvable
 
 	private Particles FireParticle { get; set; }
 
+	public Vector3 Gravity;
+
 	public FireEntity()
 	{
 
@@ -37,6 +52,24 @@ public class FireEntity : ModelEntity, IResolvable
 		Position = startPosition;
 		_moveDirection = moveDirection;
 		Velocity = _moveDirection * Time.Delta * 10f;
+
+		Gravity = Game.PhysicsWorld.Gravity * Time.Delta / 10f;
+	}
+
+	public FireEntity( Vector3 startPosition, Vector3 moveDirection, Vector3 gravity )
+	{
+		Position = startPosition;
+		_moveDirection = moveDirection;
+		Velocity = _moveDirection * Time.Delta * 10f;
+
+		if ( gravity == Vector3.Zero )
+		{
+			Gravity = Game.PhysicsWorld.Gravity * Time.Delta / 10f;
+		}
+		else
+		{
+			Gravity = gravity;
+		}
 	}
 
 	public override void Spawn()
@@ -66,7 +99,7 @@ public class FireEntity : ModelEntity, IResolvable
 	private void Move()
 	{
 		Velocity += _moveDirection * Time.Delta / 2f;
-		Velocity += Game.PhysicsWorld.Gravity * Time.Delta / 10f;
+		Velocity += Gravity;
 		Velocity += GamemodeSystem.Instance.ActiveWindForce * 128f * Time.Delta;
 
 		Velocity = Velocity.WithY( 0f );
@@ -90,6 +123,8 @@ public class FireEntity : ModelEntity, IResolvable
 		if ( collisionTrace.Hit )
 		{
 			Velocity *= 0.95f;
+
+			_moveDirection *= 0.95f;
 
 			if ( collisionTrace.Entity is not null && !collisionTrace.Entity.Tags.Has( Tag.Invincible ) )
 				collisionTrace.Entity.TakeDamage( DamageInfoExtension.FromExplosion( 0.25f, Position, Vector3.Up * 32f, this ) );
