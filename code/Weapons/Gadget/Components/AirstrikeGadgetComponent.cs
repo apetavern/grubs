8f542087be5a861f5cb7494f5d2bee6a7c74d308
dch <>
2023-06-02 01:38:22 -0500

    Play bomb door open animation

--- code/Weapons/Gadget/Components/AirstrikeGadgetComponent.cs
+++ code/Weapons/Gadget/Components/AirstrikeGadgetComponent.cs
@@ -23,18 +23,24 @@ public partial class AirstrikeGadgetComponent : GadgetComponent
 	public override void Simulate( IClient client )
 	{
 		Gadget.Position += RightToLeft ? Vector3.Backward * _speed : Vector3.Forward * _speed;
+		var currentDistanceFromTarget = Gadget.Position.WithY( 0 ).WithZ( 0 ).Distance( TargetPosition.WithY( 0 ).WithZ( 0 ) );
 
-		if ( !Game.IsServer )
-			return;
+		// Start the animation a bit early to give it enough time.
+		if ( currentDistanceFromTarget <= 300 )
+			Gadget.SetAnimParameter( "open", true );
 
-		if ( Gadget.Position.WithY( 0 ).WithZ( 0 ).Distance( TargetPosition.WithY( 0 ).WithZ( 0 ) ) <= 200 && !HasReachedTarget )
+		if ( currentDistanceFromTarget <= 200 && !HasReachedTarget )
 		{
 			HasReachedTarget = true;
-			Gadget.ShouldCameraFollow = false;
-			DropPayload();
+
+			if ( Game.IsServer )
+			{
+				Gadget.ShouldCameraFollow = false;
+				DropPayload();
+			}
 		}
 
-		if ( HasReachedTarget && Gadget.Position.WithY( 0 ).WithZ( 0 ).Distance( GrubsGame.Instance.Terrain.Position.WithY( 0 ).WithZ( 0 ) ) >= GrubsConfig.TerrainLength * 3 )
+		if ( Game.IsServer && (HasReachedTarget && Gadget.Position.WithY( 0 ).WithZ( 0 ).Distance( GrubsGame.Instance.Terrain.Position.WithY( 0 ).WithZ( 0 ) ) >= GrubsConfig.TerrainLength * 3) )
 			Gadget.Delete();
 	}
 
