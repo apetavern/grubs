2661ff35c57698df9e7a48a4c82d0041d0cbef70
Matthew Zegar <matthewzegar@gmail.com>
2023-06-05 15:46:24 -0700

    Move count check to world panel

--- code/Weapons/Components/InputHintComponent.cs
+++ code/Weapons/Components/InputHintComponent.cs
@@ -8,11 +8,4 @@ public partial class InputHintComponent : WeaponComponent
 
 	[Prefab, Net]
 	public IList<string> InputDescriptions { get; set; }
-
-	protected override void OnActivate()
-	{
-		// We can't use a dictionary since they are not supported in the Prefab Editor.
-		// Basically we need to ensure for every input action we have a description.
-		Assert.True( InputActions.Count == InputDescriptions.Count );
-	}
 }
