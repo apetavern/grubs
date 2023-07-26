6f0dee98c29209e0458e9ee76e66fd5ebdae1ec4
Drew Britt <drewbritt02@gmail.com>
2023-07-26 14:38:31 -0500

    Re-rewrite menu stats (now using stats get API)
    - Still a lil stinky as we rely on a static int to represent the length of instanced arrays, (that are all the same size) because we want unique values per Stat entry

--- code/Utils/Extensions/IClientExtensions.cs
+++ code/Utils/Extensions/IClientExtensions.cs
@@ -4,7 +4,7 @@ public static class IClientExtensions
 {
 	/// <summary>
 	/// Gets the value of a leaderboard entry for a given client.
-	/// If the client is a bot or does not have an entry, 0 is returned.
+	/// If the client is invalid, a bot, or does not have an entry, 0 is returned.
 	/// </summary>
 	/// <param name="leaderboard">Ident of the leaderboard to fetch.</param>
 	public static async Task<double> GetLeaderboardEntry( this IClient client, string leaderboard )
@@ -20,5 +20,16 @@ public static class IClientExtensions
 		if ( entry.SteamId != steamId ) return 0f;
 
 		return entry.Value;
+	}
+
+	/// <summary>
+	/// Get all stats (even ones with no entries) for this client.
+	/// If the client is invalid or a bot, null is returned.
+	/// </summary>
+	public static Sandbox.Services.Stats.PlayerStats GetPlayerStats( this IClient client )
+	{
+		if ( !client.IsValid() || client.IsBot ) return null;
+
+		return Sandbox.Services.Stats.GetPlayerStats( "apetavern.grubs", client.SteamId );
 	}
 }
