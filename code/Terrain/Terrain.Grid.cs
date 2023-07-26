c331818c08dbd974bc4c9b1597880e8cec4aee9a
yuberee <59583743+yuberee@users.noreply.github.com>
2023-07-26 21:58:32 +0200

    Generate 1 row of cells instead of 2

--- code/Terrain/Terrain.Grid.cs
+++ code/Terrain/Terrain.Grid.cs
@@ -6,7 +6,7 @@ public partial class Terrain
 {
 	public static int WorldLength => GamemodeSystem.Instance.Terrain.WorldTextureLength;
 	public static int WorldHeight => GamemodeSystem.Instance.Terrain.WorldTextureHeight;
-	public static BBox WorldBox => new BBox( new Vector3( WorldLength / 2f, 10f, WorldHeight ), new Vector3( -WorldLength / 2f, -10f, 0 ) );
+	public static BBox WorldBox => new BBox( new Vector3( WorldLength / 2f, 10f, WorldHeight ), new Vector3( -WorldLength / 2f, 0, 0 ) );
 	public static JumpDefinition NormalJump = new JumpDefinition( "jump", 125f, 240f, ControllerMechanic.Gravity );
 	public static JumpDefinition BackFlipJump = new JumpDefinition( "backflip", 50f, 240f * 1.75f, ControllerMechanic.Gravity );
 
@@ -16,7 +16,6 @@ public partial class Terrain
 			.WithBounds( Vector3.Zero, WorldBox, Rotation.Identity )
 			.WithHeightClearance( GrubController.EyeHeight )
 			.WithWidthClearance( GrubController.BodyGirth )
-			.WithStaticOnly( false )
 			.WithEdgeNeighbourCount( 2 )
 			.WithStepSize( SquirmMechanic.StepSize )
 			.WithStandableAngle( SquirmMechanic.GroundAngle )
@@ -30,6 +29,6 @@ public partial class Terrain
 	[ConCmd.Admin("grub_grid")]
 	static void GrubGridDebug()
 	{
-		GenerateGrid();
+		GenerateGrid().Wait();
 	}
 }
