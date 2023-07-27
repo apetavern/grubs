6e546bac38cce767d8db598f258eb4f19f1a7f98
yuberee <59583743+yuberee@users.noreply.github.com>
2023-07-27 22:05:05 +0200

    2D Jumps + Fix the drop height

--- code/Terrain/Terrain.Grid.cs
+++ code/Terrain/Terrain.Grid.cs
@@ -7,8 +7,8 @@ public partial class Terrain
 	public static int WorldLength => GamemodeSystem.Instance.Terrain.WorldTextureLength;
 	public static int WorldHeight => GamemodeSystem.Instance.Terrain.WorldTextureHeight;
 	public static BBox WorldBox => new BBox( new Vector3( WorldLength / 2f, 10f, WorldHeight ), new Vector3( -WorldLength / 2f, 0, 0 ) );
-	public static JumpDefinition NormalJump = new JumpDefinition( "jump", 125f, 240f, ControllerMechanic.Gravity );
-	public static JumpDefinition BackFlipJump = new JumpDefinition( "backflip", 50f, 240f * 1.75f, ControllerMechanic.Gravity );
+	public static JumpDefinition NormalJump = new JumpDefinition( "jump", 125f, 240f, ControllerMechanic.Gravity, 2 );
+	public static JumpDefinition BackFlipJump = new JumpDefinition( "backflip", 50f, 240f * 1.75f, ControllerMechanic.Gravity, 2 );
 
 	public static async Task GenerateGrid()
 	{
@@ -20,7 +20,7 @@ public partial class Terrain
 			.WithEdgeNeighbourCount( 2 )
 			.WithStepSize( SquirmMechanic.StepSize )
 			.WithStandableAngle( SquirmMechanic.GroundAngle )
-			.WithMaxDropHeight( 80 )
+			.WithMaxDropHeight( 150 )
 			.AddJumpDefinition( NormalJump )
 			.AddJumpDefinition( BackFlipJump );
 
