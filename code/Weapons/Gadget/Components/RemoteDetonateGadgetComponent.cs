310afdd4a93303b5355d06b6db1fbf7fc28bb664
Drew Britt <drewbritt02@gmail.com>
2023-08-09 19:02:03 -0500

    Fix goat explosion :goat:

--- code/Weapons/Gadget/Components/RemoteDetonateGadgetComponent.cs
+++ code/Weapons/Gadget/Components/RemoteDetonateGadgetComponent.cs
@@ -3,15 +3,17 @@ namespace Grubs;
 [Prefab]
 public partial class RemoteDetonateGadgetComponent : GadgetComponent
 {
+	private TimeSince _creation = 0;
+
 	public override void ClientSpawn()
 	{
 		if ( Gadget.Owner.IsLocalPawn )
-			_ = new UI.InputHintWorldPanel( Gadget, new List<string>() { InputAction.CameraPan }, new List<string>() { "Explode" } );
+			_ = new UI.InputHintWorldPanel( Gadget, new List<string>() { InputAction.Fire }, new List<string>() { "Explode" } );
 	}
 
 	public override void Simulate( IClient client )
 	{
-		if ( Input.Down( InputAction.CameraPan ) )
+		if ( Input.Pressed( InputAction.Fire ) && _creation > 0.1f )
 			Gadget.Components.Get<ExplosiveGadgetComponent>()?.Explode();
 	}
 }
