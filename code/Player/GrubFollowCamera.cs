c35bcf7fee4d3f59a9dcab0a3b0352cfa4ffed09
trundlr <57276947+trundlr@users.noreply.github.com>
2024-01-25 19:15:37 -0400

    Camera follows grub for testing purposes

---
+++ code/Player/GrubFollowCamera.cs
@@ -0,0 +1,15 @@
+﻿namespace Grubs.Player;
+
+public class GrubFollowCamera : Component
+{
+	[Property] public required GameObject Target { get; set; }
+
+	protected override void OnUpdate()
+	{
+		var cam = GameObject;
+		var targetPos = Target.Transform.Position;
+		targetPos.y = cam.Transform.Position.y;
+		targetPos.z += 32f;
+		cam.Transform.Position = Vector3.Lerp( cam.Transform.Position, targetPos, 0.1f );
+	}
+}
