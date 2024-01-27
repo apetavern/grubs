455d8c29a2a6075a19f8d160d167afd22a105860
trundlr <57276947+trundlr@users.noreply.github.com>
2024-01-27 16:31:11 -0400

    GrubFollowCamera: Scroll, adjust Highlight width

--- code/Player/GrubFollowCamera.cs
+++ code/Player/GrubFollowCamera.cs
@@ -4,12 +4,32 @@ public class GrubFollowCamera : Component
 {
 	[Property] public required GameObject Target { get; set; }
 
+	public float Distance { get; set; } = 1024f;
+
 	protected override void OnUpdate()
 	{
 		var cam = GameObject;
-		var targetPos = Target.Transform.Position;
-		targetPos.y = cam.Transform.Position.y;
+		var targetPos = Target.Transform.Position + Vector3.Right * Distance;
 		targetPos.z += 32f;
-		cam.Transform.Position = Vector3.Lerp( cam.Transform.Position, targetPos, 0.1f );
+		cam.Transform.Position = cam.Transform.Position.LerpTo( targetPos, Time.Delta * 5f );
+	}
+
+	protected override void OnFixedUpdate()
+	{
+		base.OnFixedUpdate();
+
+		Distance -= Input.MouseWheel.y * 32f;
+		Distance = Distance.Clamp( 128f, 2048f );
+
+		AdjustHighlightOutline();
+	}
+
+	private void AdjustHighlightOutline()
+	{
+		if ( !Target.Components.TryGet( out HighlightOutline highlight ) )
+			return;
+
+		var desiredWidth = (1 / Distance * 100).Clamp( 0f, 0.35f );
+		highlight.Width = highlight.Width.LerpTo( desiredWidth, Time.Delta * 5f );
 	}
 }
