f0f01fd665cd8682ad70223fd55cea9bfa91780a
trundlr <57276947+trundlr@users.noreply.github.com>
2024-01-31 18:55:57 -0400

    ExplosiveProjectile: more properties, explode after seconds

--- code/Equipment/Weapons/ExplosiveProjectileComponent.cs
+++ code/Equipment/Weapons/ExplosiveProjectileComponent.cs
@@ -2,19 +2,43 @@
 
 namespace Grubs.Equipment.Weapons;
 
-[Title( "Grub - Explosive Projectile" )]
-[Category( "Equipment" )]
+[Title( "Grub - Explosive Projectile" ), Category( "Equipment" )]
 public class ExplosiveProjectileComponent : Component
 {
-	[Property] public float ExplosionDamage { get; set; } = 50f;
+	[Property] private float ExplosionDamage { get; set; } = 50f;
+	[Property] private float ExplosionRadius { get; set; } = 100f;
 	[Property] public bool ExplodeOnCollision { get; set; } = false;
 	[Property] public bool DeleteOnExplode { get; set; } = true;
-	[Property] [Sync] public float ExplodeAfter { get; set; } = 4.0f;
+	[Property, Sync] public float ExplodeAfter { get; set; } = 4.0f;
+	[Property, ResourceType( "sound" )] private string ExplosionSound { get; set; } = "";
+
+	[Net] private TimeUntil TimeUntilExplosion { get; set; }
+
+	protected override void OnStart()
+	{
+		if ( ExplodeAfter > 0f )
+		{
+			TimeUntilExplosion = ExplodeAfter;
+			ExplodeAfterSeconds( ExplodeAfter );
+		}
+	}
+
+	private async void ExplodeAfterSeconds( float seconds )
+	{
+		await GameTask.DelaySeconds( seconds );
+
+		if ( !GameObject.IsValid )
+			return;
+
+		Explode();
+	}
 
 	public void Explode()
 	{
-		Sound.Play( "explosion_short_tail" );
-		ExplosionHelperComponent.Instance.Explode( this, Transform.Position, 100f, 50f );
-		GameObject.Destroy();
+		Sound.Play( ExplosionSound );
+		ExplosionHelperComponent.Instance.Explode( this, Transform.Position, ExplosionRadius, ExplosionDamage );
+
+		if ( DeleteOnExplode )
+			GameObject.Destroy();
 	}
 }
