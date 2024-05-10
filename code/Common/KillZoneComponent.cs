bb66e4ac4bf5bf8b7f48175ed27f358d6ca8513d
trundlr <57276947+trundlr@users.noreply.github.com>
2024-05-10 10:34:36 -0300

    Destroy projectiles in killzone, hack to prevent 0,0,0 cull

--- code/Common/KillZoneComponent.cs
+++ code/Common/KillZoneComponent.cs
@@ -9,6 +9,9 @@ public class KillZoneComponent : Component, Component.ITriggerListener
 	{
 		if ( other.GameObject.Components.TryGet( out Grub grub, FindMode.EverythingInSelfAndAncestors ) )
 			grub.Health.TakeDamage( GrubsDamageInfo.FromKillZone( 9999 ), true );
+
+		if ( other.GameObject.Tags.Has( "projectile" ) && other.Transform.Position != 0f )
+			other.GameObject.Destroy();
 	}
 
 	public void OnTriggerExit( Collider other )
