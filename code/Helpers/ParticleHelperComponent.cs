7fa3484753e3b5c333a3451231a2e691e7387710
Cameron <805camerons@gmail.com>
2024-05-13 10:50:42 -0400

    Add optional radius arg to PlayInstantaneous

--- code/Helpers/ParticleHelperComponent.cs
+++ code/Helpers/ParticleHelperComponent.cs
@@ -16,10 +16,14 @@ public sealed class ParticleHelperComponent : Component
 		return PlayInstantaneous( particles, global::Transform.Zero );
 	}
 
-	public SceneParticles PlayInstantaneous( ParticleSystem particles, Transform transform )
+	public SceneParticles PlayInstantaneous( ParticleSystem particles, Transform transform, float radius=0 )
 	{
 		var sceneObject = new SceneParticles( Scene.SceneWorld, particles );
 		sceneObject.SetControlPoint( 0, transform );
+
+		if (radius > 0)
+			sceneObject.SetControlPoint( 1, new Vector3(radius, 0, 0) );
+
 		sceneObject.Transform = transform;
 
 		_sceneObjects.Add( sceneObject );
