04a37bdacfd90bc62993e677d378eadad54c5043
Cameron <805camerons@gmail.com>
2024-05-13 10:57:05 -0400

    Revert changes to ParticleHelperComponent

--- code/Helpers/ParticleHelperComponent.cs
+++ code/Helpers/ParticleHelperComponent.cs
@@ -16,14 +16,10 @@ public sealed class ParticleHelperComponent : Component
 		return PlayInstantaneous( particles, global::Transform.Zero );
 	}
 
-	public SceneParticles PlayInstantaneous( ParticleSystem particles, Transform transform, float radius=0 )
+	public SceneParticles PlayInstantaneous( ParticleSystem particles, Transform transform )
 	{
 		var sceneObject = new SceneParticles( Scene.SceneWorld, particles );
 		sceneObject.SetControlPoint( 0, transform );
-
-		if (radius > 0)
-			sceneObject.SetControlPoint( 1, new Vector3(radius, 0, 0) );
-
 		sceneObject.Transform = transform;
 
 		_sceneObjects.Add( sceneObject );
