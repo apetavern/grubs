6a0cdfad7475d2f90b1646fd69fc5b0d499b349c
Brian Lindenhof <brian@shadowbrain.net>
2024-05-24 21:55:22 +0200

    Initial blowtorch scaffolding

---
+++ code/Equipment/Weapons/TorchWeapon.cs
@@ -0,0 +1,14 @@
+using Grubs.Equipment.Weapons;
+using Sandbox;
+
+namespace Grubs;
+
+[Title( "Grubs - Torch Weapon" ), Category( "Equipment" )]
+public sealed class TorchWeapon : Weapon
+{
+	protected override void HandleComplexFiringInput()
+	{
+		base.HandleComplexFiringInput();
+
+	}
+}
