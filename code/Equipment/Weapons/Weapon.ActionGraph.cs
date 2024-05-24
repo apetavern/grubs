033074ba247cb50a19a4f716e64ce9e1569f0c33
Brian Lindenhof <brian@shadowbrain.net>
2024-05-24 22:45:59 +0200

    Remove extra actiongraph node

--- code/Equipment/Weapons/Weapon.ActionGraph.cs
+++ code/Equipment/Weapons/Weapon.ActionGraph.cs
@@ -5,7 +5,7 @@ namespace Grubs.Equipment.Weapons;
 public partial class Weapon
 {
 	[ActionGraphNode( "grubs.spawn_projectile" ), Title( "Spawn Projectile" ), Group( "Grubs Actions" )]
-	public static void SpawnProjectile( Weapon source, GameObject projectile, int charge )
+	public static GameObject SpawnProjectile( Weapon source, GameObject projectile, int charge )
 	{
 		var go = projectile.Clone();
 		go.NetworkSpawn();
@@ -16,22 +16,7 @@ public partial class Weapon
 		}
 
 		source.TimeSinceLastUsed = 0f;
-	}
-
-	[ActionGraphNode( "grubs.spawn_homing_projectile" ), Title( "Spawn Homing Projectile" ), Group( "Grubs Actions" )]
-	public static void SpawnHomingProjectile( HomingWeapon source, GameObject projectile, int charge )
-	{
-		var go = projectile.Clone();
-		go.NetworkSpawn();
-		if ( go.Components.TryGet( out HomingProjectile pc ) )
-		{
-			pc.Source = source;
-			pc.Charge = charge;
-			pc.PassDataOn();
-			pc.ProjectileTarget = source.ProjectileTarget;
-		}
-
-		source.TimeSinceLastUsed = 0f;
+		return go;
 	}
 
 	[ActionGraphNode( "grubs.fire_finished" ), Title( "Fire Finished" ), Group( "Grubs Actions" )]
