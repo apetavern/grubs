bf0f0f92a8dbcc6ebaa3ef5601293fa43ab53e93
Brian Lindenhof <brian@shadowbrain.net>
2024-05-24 20:31:10 +0200

    Initial homing weapon setup with homing projectile action graph action

---
+++ code/Equipment/Gadgets/Projectiles/HomingProjectile.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Grubs.Equipment.Gadgets.Projectiles;
+public class HomingProjectile : Projectile
+{
+	public Vector3 ProjectileTarget { get; set; }
+	[Property] Projectile ProjectileMovement { get; set; }
+
+	public void PassDataOn()
+	{
+		ProjectileMovement.Source = Source;
+		ProjectileMovement.Charge = Charge;
+		StartHoming();
+	}
+
+	public async void StartHoming()
+	{
+		await Task.DelaySeconds( 1f );
+		if(ProjectileMovement is PhysicsProjectile pp )
+		{
+			pp.PhysicsBody.Gravity = false;
+			pp.PhysicsBody.AngularDamping = 5f;
+			while ( Vector3.DistanceBetween(Transform.Position, ProjectileTarget) > 10f )
+			{
+				await Task.FixedUpdate();
+				pp.PhysicsBody.Velocity = Transform.Rotation.Forward * ProjectileSpeed;
+				Transform.Rotation = Rotation.Lerp(Transform.Rotation, Rotation.LookAt( ProjectileTarget - Transform.Position ), 2f * Time.Delta);
+			}
+		}
+	}
+
+
+}
--- code/Equipment/Weapons/Weapon.ActionGraph.cs
+++ code/Equipment/Weapons/Weapon.ActionGraph.cs
@@ -18,6 +18,22 @@ public partial class Weapon
 		source.TimeSinceLastUsed = 0f;
 	}
 
+	[ActionGraphNode( "grubs.spawn_homing_projectile" ), Title( "Spawn Homing Projectile" ), Group( "Grubs Actions" )]
+	public static void SpawnHomingProjectile( HomingWeapon source, GameObject projectile, int charge )
+	{
+		var go = projectile.Clone();
+		go.NetworkSpawn();
+		if ( go.Components.TryGet( out HomingProjectile pc ) )
+		{
+			pc.Source = source;
+			pc.Charge = charge;
+			pc.PassDataOn();
+			pc.ProjectileTarget = source.ProjectileTarget;
+		}
+
+		source.TimeSinceLastUsed = 0f;
+	}
+
 	[ActionGraphNode( "grubs.fire_finished" ), Title( "Fire Finished" ), Group( "Grubs Actions" )]
 	public static void FireFinished( Weapon source )
 	{
