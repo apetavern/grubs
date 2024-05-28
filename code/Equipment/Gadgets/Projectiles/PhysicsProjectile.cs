7f7639a6a7f5b2a6df86a51aaeeb1f336f6b7319
Brian Lindenhof <brian@shadowbrain.net>
2024-05-28 14:26:49 +0200

    Add "SetRotationOnStart" to physics projectiles

--- code/Equipment/Gadgets/Projectiles/PhysicsProjectile.cs
+++ code/Equipment/Gadgets/Projectiles/PhysicsProjectile.cs
@@ -6,6 +6,7 @@ public class PhysicsProjectile : Projectile
 	[Property] public bool Droppable { get; set; } = false;
 	[Property] public required Rigidbody PhysicsBody { get; set; }
 	[Property] public bool SetPositionOnStart { get; set; } = true;
+	[Property] public bool SetRotationOnStart { get; set; } = false;
 
 	public override bool Resolved => PhysicsBody.Velocity.IsNearlyZero( 0.1f );
 
@@ -28,6 +29,12 @@ public class PhysicsProjectile : Projectile
 			Transform.Position += dir * 16f;
 			PhysicsBody.ApplyImpulseAt( PhysicsBody.Transform.Position + Vector3.Up * 0.5f,
 				dir * Charge * ProjectileSpeed );
+		}
+
+		if ( SetRotationOnStart )
+		{
+			var dir = PlayerController.EyeRotation.Forward.Normal * PlayerController.Facing;
+			Transform.Rotation = Rotation.LookAt( dir, Vector3.Up );
 		}
 	}
 }
