54805f2c4e4dd8d8eaf132a0c849119c3c24cad2
Brian Lindenhof <brian@shadowbrain.net>
2024-05-30 13:43:31 +0200

    Update bouncy component to also allow for naive reversing of velocity instead

--- code/Equipment/Gadgets/Projectiles/Bouncy.cs
+++ code/Equipment/Gadgets/Projectiles/Bouncy.cs
@@ -7,11 +7,12 @@ public sealed class Bouncy : Component, ICollisionListener
 {
 	[Property] public Rigidbody Body { get; set; }
 	[Property] public float DampingFactor { get; set; } = 0.8f;
+	[Property] private bool Reflect { get; set; } = true;
 
 	public void OnCollisionStart( Collision other )
 	{
 		var speed = other.Contact.Speed.Length;
-		var direction = Vector3.Reflect( other.Contact.Speed.Normal, other.Contact.Normal );
+		var direction = Reflect ? Vector3.Reflect( other.Contact.Speed.Normal, other.Contact.Normal ) : -Body.Velocity.Normal;
 		Body.Velocity = direction * speed * DampingFactor;
 	}
 }
