9bafe3a4ce935f3c1ec441703cfda2051d4a4a0e
Brian Lindenhof <brian@shadowbrain.net>
2024-06-01 10:25:40 +0200

    Make Spinning properties into a single property

--- code/Equipment/Gadgets/Projectiles/Spinning.cs
+++ code/Equipment/Gadgets/Projectiles/Spinning.cs
@@ -4,35 +4,12 @@ namespace Grubs;
 
 public sealed class Spinning : Component
 {
-	[Property] public float SpinSpeed { get; set; } = 1f;
-	[Property] public RotationAxis SpinAxis { get; set; } = RotationAxis.Yaw;
+	[Property] public Angles RotationSpeed { get; set; }
 
 	protected override void OnFixedUpdate()
 	{
 		base.OnFixedUpdate();
 
-		var rotation = Transform.Rotation;
-
-		switch ( SpinAxis )
-		{
-			case RotationAxis.Pitch:
-				rotation *= Rotation.FromPitch( SpinSpeed * Time.Delta );
-				break;
-			case RotationAxis.Yaw:
-				rotation *= Rotation.FromYaw( SpinSpeed * Time.Delta );
-				break;
-			case RotationAxis.Roll:
-				rotation *= Rotation.FromRoll( SpinSpeed * Time.Delta );
-				break;
-		}
-
-		Transform.Rotation = rotation;
+		Transform.Rotation *= RotationSpeed * Time.Delta;
 	}
-}
-
-public enum RotationAxis
-{
-	Pitch,
-	Yaw,
-	Roll
 }
