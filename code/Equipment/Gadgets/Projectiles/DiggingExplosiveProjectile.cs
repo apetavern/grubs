06ec9d9b5f452b6b72680f808dbb7ea067f57ada
Cameron <805camerons@gmail.com>
2024-06-02 12:08:55 -0400

    Change Bunker Buster to be scripted movement
    Fixes #374

--- code/Equipment/Gadgets/Projectiles/DiggingExplosiveProjectile.cs
+++ code/Equipment/Gadgets/Projectiles/DiggingExplosiveProjectile.cs
@@ -9,11 +9,14 @@ public sealed class DiggingExplosiveProjectile : TargetedProjectile
 	[Property] public float DigWidth { get; set; } = 10f;
 	[Property] public float DigLength { get; set; } = 50f;
 	[Property] public ExplosiveProjectile Explosive { get; set; }
-	[Property] public PhysicsProjectile Physics { get; set; }
+	[Property] public Rigidbody Physics { get; set; }
+	[Property] public ScriptedMovementProjectile Movement { get; set; }
 	[Property, Description( "The seconds after making contact with terrain before exploding" )] public float TimeBeforeDetonation { get; set; } = 4f;
+	public bool IsDigging => _timeSinceDug < 0.2f;
 
 	private bool _isArmed = false; // Whether the projectile has made contact with terrain
 	private TimeSince _timeSinceArmed = 0f;
+	private TimeSince _timeSinceDug = 0f;
 
 	public override void ShareData()
 	{
@@ -21,14 +24,14 @@ public sealed class DiggingExplosiveProjectile : TargetedProjectile
 		Transform.Position = ProjectileTarget.WithZ( GrubsConfig.TerrainHeight * 2f );
 	}
 
-	protected override void OnUpdate()
+	protected override void OnFixedUpdate()
 	{
-		base.OnUpdate();
+		base.OnFixedUpdate();
 
 		var startPos = Transform.Position;
 		var endPos = startPos + Vector3.Down * DigLength;
 
-		var tr = Scene.Trace.Body( Physics.PhysicsBody.PhysicsBody, endPos )
+		var tr = Scene.Trace.Body( Physics.PhysicsBody, endPos )
 			.WithAnyTags( "player", "solid" )
 			.WithoutTags( "dead" )
 			.IgnoreGameObjectHierarchy( GameObject.Root )
@@ -44,11 +47,21 @@ public sealed class DiggingExplosiveProjectile : TargetedProjectile
 					_timeSinceArmed = 0f;
 					_isArmed = true;
 				}
+
+				_timeSinceDug = 0f;
+				Movement.OverrideMovement = Movement.Movement / 4f;
 			}
 			else
 			{
 				Explosive.Explode();
 				return;
+			}
+		}
+		else
+		{
+			if ( _timeSinceDug > 0.2f )
+			{
+				Movement.OverrideMovement = Vector3.Zero;
 			}
 		}
 
