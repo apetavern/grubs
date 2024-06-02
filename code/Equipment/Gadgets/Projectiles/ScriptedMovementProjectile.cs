9d32e8c0afc787c5cedc0aa50cad694e41c46e6e
Cameron <805camerons@gmail.com>
2024-06-02 12:06:41 -0400

    Add ScriptedMovementProjectile

---
+++ code/Equipment/Gadgets/Projectiles/ScriptedMovementProjectile.cs
@@ -0,0 +1,22 @@
+﻿namespace Grubs.Equipment.Gadgets.Projectiles;
+
+[Title( "Grubs - Scripted Movement Projectile" ), Category( "Equipment" )]
+public class ScriptedMovementProjectile : Projectile
+{
+	[Property] public Vector3 Movement { get; set; }
+
+	[Property] public Vector3 OverrideMovement { get; set; } // Set externally if we need to change the movement temporarily
+
+	protected override void OnFixedUpdate()
+	{
+		base.OnFixedUpdate();
+
+		if ( OverrideMovement != Vector3.Zero )
+		{
+			Transform.Position += OverrideMovement * Time.Delta;
+			return;
+		}
+
+		Transform.Position += Movement * Time.Delta;
+	}
+}
