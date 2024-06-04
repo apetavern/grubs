dcfe6d9010d63c9fef17645593c190af290e8ff7
Cameron <805camerons@gmail.com>
2024-06-04 18:51:34 -0400

    Remove unused Shuffle method

--- code/Extensions/IEnumerableExtensions.cs
+++ code/Extensions/IEnumerableExtensions.cs
@@ -11,15 +11,4 @@ public static class ListExtensions
 
 		return result;
 	}
-
-	public static void Shuffle<T>( this IList<T> list )
-	{
-		var n = list.Count;
-		while ( n > 1 )
-		{
-			n--;
-			var k = Random.Shared.Next( n + 1 );
-			(list[n], list[k]) = (list[k], list[n]);
-		}
-	}
 }
