6b7dcbf4ce1845aaf1716f9e43d913c8e597614a
Drew Britt <drewbritt02@gmail.com>
2024-06-07 01:53:40 -0500

    Skip UseTurn task if already called; simplify call; Grub resolving failsafe

--- code/Equipment/Weapons/Weapon.cs
+++ code/Equipment/Weapons/Weapon.cs
@@ -162,8 +162,7 @@ public partial class Weapon : Component
 			if ( !CanSwapAfterUse )
 			{
 				grub.Player.HasFiredThisTurn = true;
-				using ( Rpc.FilterInclude( c => c.IsHost ) )
-					Gamemode.FFA.UseTurn( true );
+				Gamemode.FFA.UseTurn( true );
 			}
 			else
 			{
