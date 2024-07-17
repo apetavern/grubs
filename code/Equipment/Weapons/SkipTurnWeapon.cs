51310e6c8eb5439850d0a4a09617f99039c22741
Drew Britt <drewbritt02@gmail.com>
2024-07-16 19:10:05 -0500

    Smoke Break tool to skip turn

---
+++ code/Equipment/Weapons/SkipTurnWeapon.cs
@@ -0,0 +1,31 @@
+﻿using Grubs.Helpers;
+
+namespace Grubs.Equipment.Weapons
+{
+	[Title( "Grubs - Skip Turn Weapon" ), Category( "Equipment" )]
+	public class SkipTurnWeapon : Weapon
+	{
+		[Property] public ParticleSystem UseParticles { get; set; }
+
+		protected override void FireImmediate()
+		{
+			if ( Equipment.Grub is not { } grub )
+				return;
+
+			FireEffects();
+			FireFinished();
+		}
+
+		[Broadcast]
+		private void FireEffects()
+		{
+			var muzzle = GetMuzzlePosition();
+
+			if ( UseParticles is not null )
+			{
+				var smoke = ParticleHelper.Instance.PlayInstantaneous( UseParticles, muzzle );
+				smoke.SetControlPoint( 1, 2f ); // Rise distance
+			}
+		}
+	}
+}
