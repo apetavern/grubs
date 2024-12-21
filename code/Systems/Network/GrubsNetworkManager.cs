fb04debf1ae91ef5361f2d1de831e3c63815a312
trundlr <57276947+trundlr@users.noreply.github.com>
2024-12-21 15:32:31 -0400

    LocalComponent, Player and Client components from scratch

---
+++ code/Systems/Network/GrubsNetworkManager.cs
@@ -0,0 +1,43 @@
+﻿using Grubs.Systems.Pawn;
+
+namespace Grubs.Systems.Network;
+
+[Title( "Grubs - Network Manager" ), Category("Grubs/Network")]
+public sealed class GrubsNetworkManager : Component, Component.INetworkListener
+{
+	[Property] public required GameObject ClientPrefab { get; set; }
+	
+	protected override void OnStart()
+	{
+		if ( Networking.IsActive ) 
+			return;
+		
+		var lobbyConfig = new LobbyConfig
+		{
+			DestroyWhenHostLeaves = false,
+			AutoSwitchToBestHost = true,
+			MaxPlayers = 8,
+			Privacy = LobbyPrivacy.Public,
+		};
+		Log.Info( "Creating new Grubs lobby." );
+		Networking.CreateLobby( lobbyConfig );
+	}
+
+	public void OnActive( Connection connection )
+	{
+		Log.Info( $"Spawning client prefab for connection {connection.DisplayName} ({connection.Id})." );
+		var clientObj = ClientPrefab.Clone();
+		clientObj.NetworkSpawn( connection );
+		
+		Log.Info( $"Assigning connection {connection.Id} to Client component." );
+		var client = clientObj.GetComponent<Client>();
+		client.OnNetworkActive( connection );
+	}
+
+	protected override void OnUpdate()
+	{
+		base.OnUpdate();
+		
+		Log.Info( Client.GetLocal() );
+	}
+}
---
+++ code/Systems/Pawn/Client.cs
@@ -0,0 +1,39 @@
+﻿using Grubs.Common;
+
+namespace Grubs.Systems.Pawn;
+
+[Title( "Client" ), Category( "Grubs/Pawn" )]
+public sealed class Client : LocalComponent<Client>
+{
+	[Property]
+	private GameObject PlayerPrefab { get; set; }
+	
+	[Sync( SyncFlags.FromHost )] 
+	public Guid ConnectionId { get; set; }
+	
+	public Connection Connection => Connection.Find( ConnectionId );
+
+	protected override void OnStart()
+	{
+		if ( IsProxy )
+			return;
+
+		Local = this;
+	}
+	
+	public void OnNetworkActive( Connection connection )
+	{
+		ConnectionId = connection.Id;
+		
+		var playerObj = PlayerPrefab.Clone();
+		playerObj.NetworkSpawn();
+		
+		var player = playerObj.GetComponent<Player>();
+		player.SetClient( this );
+	}
+	
+	public override string ToString()
+	{
+		return $"Client ({Connection.DisplayName}) on {GameObject.Name}";
+	}
+}
