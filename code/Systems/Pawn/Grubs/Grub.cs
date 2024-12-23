349c6efff6825948dde441c9c058c91ecfe240a0
trundlr <57276947+trundlr@users.noreply.github.com>
2024-12-23 16:55:51 -0400

    Initialize game state, give Grub an Owner

--- code/Systems/Pawn/Grubs/Grub.cs
+++ code/Systems/Pawn/Grubs/Grub.cs
@@ -3,5 +3,18 @@ namespace Grubs.Systems.Pawn.Grubs;
 [Title("Grub"), Category("Grubs/Pawn")]
 public sealed class Grub : Component
 {
+	[Sync]
+	public Player Owner { get; private set; }
 	
+	[Rpc.Owner( NetFlags.HostOnly )]
+	public void SetOwner( Player player )
+	{
+		Owner = player;
+		Log.Info( $"Set owner of {this} to {player}." );
+	}
+
+	public override string ToString()
+	{
+		return $"Grub (Owner: {Owner})";
+	}
 }
