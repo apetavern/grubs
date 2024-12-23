1a94376ca46fe1439f053d91ad2f1321273425c2
trundlr <57276947+trundlr@users.noreply.github.com>
2024-12-23 00:07:30 -0400

    Create grubs for each player

--- code/Systems/GameMode/FreeForAll.cs
+++ code/Systems/GameMode/FreeForAll.cs
@@ -7,21 +7,53 @@ namespace Grubs.Systems.GameMode;
 public sealed class FreeForAll : BaseGameMode
 {
 	public override string Name => "Free For All";
+	
+	[Sync( SyncFlags.FromHost )]
+	public FreeForAllState State { get; set; } = FreeForAllState.Lobby;
 
 	protected override void OnModeInit()
 	{
 		Log.Info( $"{Name} mode initializing." );
+
+		State = FreeForAllState.Lobby;
 
 		GrubsTerrain.Instance.Init();
 	}
 
 	protected override void OnModeStarted()
 	{
+		if ( State is FreeForAllState.Playing )
+		{
+			Log.Warning( $"Tried to start {Name}, but State is already Playing." );
+			return;
+		}
+		
 		Log.Info( $"{Name} mode starting." );
+
+		const float grubSize = 8f;
+
+		// For each player, spawn Grubs and initialize their inventory.
+		foreach ( var player in Players )
+		{
+			for ( var i = 0; i < GrubsConfig.GrubCount; i++ )
+			{
+				var spawnLocation = GrubsTerrain.Instance.FindSpawnLocation( size: grubSize );
+				player.AddGrub( spawnLocation );
+			}
+		}
+
+		State = FreeForAllState.Playing;
 	}
 
 	protected override void OnPlayerJoined( Player player )
 	{
 		Log.Info( $"Adding {player.GameObject.Name} to Free For All game mode." );
 	}
+}
+
+public enum FreeForAllState
+{
+	Lobby,
+	Playing,
+	GameOver
 }
---
+++ code/Systems/Pawn/Grubs/Grub.cs
@@ -0,0 +1,7 @@
+namespace Grubs.Systems.Pawn.Grubs;
+
+[Title("Grub"), Category("Grubs/Pawn")]
+public sealed class Grub : Component
+{
+	
+}
