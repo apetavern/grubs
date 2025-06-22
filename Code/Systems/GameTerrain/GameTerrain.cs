7a403b1d3c82fea7f89aa5874643d8c02e93fb6c
trundlr <57276947+trundlr@users.noreply.github.com>
2025-06-22 12:11:59 -0300

    GameTerrain, CreateOrLoadLevel.razor

---
+++ Code/Systems/GameTerrain/GameTerrain.cs
@@ -0,0 +1,14 @@
+﻿using Grubs.Common;
+using Grubs.Systems.LevelEditing;
+using Sandbox.Sdf;
+
+namespace Grubs.Terrain;
+
+[Title( "Grubs - Game Terrain" ), Category( "Grubs" ), Icon( "landscape" )]
+public partial class GameTerrain : LocalComponent<GameTerrain>
+{
+	public LevelDefinition LevelDefinition { get; private set; }
+
+	[Property]
+	public Sdf2DWorld SdfWorld { get; private set; }
+}
