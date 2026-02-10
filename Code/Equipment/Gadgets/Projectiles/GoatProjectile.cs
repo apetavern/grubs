63ed749d863813670fbb1b9d7369466713f6a901
Brian Lindenhof <brian@shadowbrain.net>
2026-02-10 18:52:45 +0100

    Fix up the goat implementation

--- Code/Equipment/Gadgets/Projectiles/GoatProjectile.cs
+++ Code/Equipment/Gadgets/Projectiles/GoatProjectile.cs
@@ -1,7 +1,9 @@
-﻿namespace Grubs.Equipment.Gadgets.Projectiles;
+﻿using Sandbox;
+
+namespace Grubs.Equipment.Gadgets.Projectiles;
 
 [Title( "Grubs - Goat Projectile" ), Category( "Equipment" )]
-public class GoatProjectile : Projectile, Component.ICollisionListener
+public class GoatProjectile : Projectile
 {
 	[Property] public bool Droppable { get; set; } = false;
 	[Property] public Rigidbody PhysicsBody { get; set; }
@@ -9,6 +11,8 @@ public class GoatProjectile : Projectile, Component.ICollisionListener
 
 	[Property] private SoundEvent CollisionSound { get; set; }
 	private Vector3 TargetLookAt { get; set; }
+
+	private Rotation StartRotation { get; set; }
 
 	public override bool Resolved => PhysicsBody?.Velocity.IsNearlyZero( 0.1f ) ?? true;
 
@@ -19,14 +23,16 @@ public class GoatProjectile : Projectile, Component.ICollisionListener
 
 		WorldPosition = Source.GetStartPosition( Droppable );
 
+		var dir = Source.GetMuzzleForward() + Vector3.Up;
+		WorldPosition += dir * 16f;
+		WorldRotation = Rotation.LookAt( dir.WithZ( 0f ) );
+		StartRotation = WorldRotation; 
+
 		if ( Droppable )
 			return;
 
 		if ( !PlayerController.IsValid() )
 			return;
-
-		var dir = (PlayerController.EyeRotation.Forward.Normal * PlayerController.Facing) + Vector3.Up;
-		WorldPosition += dir * 16f;
 
 		if ( !PhysicsBody.IsValid() )
 			return;
@@ -35,33 +41,38 @@ public class GoatProjectile : Projectile, Component.ICollisionListener
 		Model.Set( "active", true );
 	}
 
+	TimeSince timeSinceLastBleat;
+
 	protected override void OnUpdate()
 	{
 		if ( !PlayerController.IsValid() || !PhysicsBody.IsValid() )
 			return;
 
 		base.OnUpdate();
+
 		Model.Set( "active", true );
-		TargetLookAt = (PlayerController.EyeRotation.Forward.Normal.WithZ( 0 ) * PlayerController.Facing) + Vector3.Up * PhysicsBody.Velocity.z / 750f;
+		TargetLookAt = StartRotation.Forward + Vector3.Up * PhysicsBody.Velocity.z / 750f;
 		WorldRotation = Rotation.Lerp( WorldRotation, Rotation.LookAt( TargetLookAt, Vector3.Up ), Time.Delta * 10f );
-	}
 
-	public void OnCollisionStart( Collision other )
-	{
-		if ( CollisionSound is not null )
-			Sound.Play( CollisionSound, WorldPosition );
-		Model.Set( "grounded", true );
+		var groundTrace = Scene.Trace.Body( PhysicsBody, WorldPosition - Vector3.Up * 5f )
+			.IgnoreGameObject(GameObject )
+			.Run();
 
-		if ( !PhysicsBody.IsValid() )
-			return;
+		var ceilingTrace = Scene.Trace.Body( PhysicsBody, WorldPosition + Vector3.Up * 5f )
+		.IgnoreGameObject( GameObject )
+		.Run();
 
-		if ( -other.Contact.Normal.z > 0.5f )
+		Model.Set( "grounded", groundTrace.Hit && !ceilingTrace.Hit );
+
+		if(groundTrace.Hit && !ceilingTrace.Hit)
+		{
+			if ( CollisionSound is not null && timeSinceLastBleat > 0.2f )
+			{
+				Sound.Play( CollisionSound, WorldPosition );
+				timeSinceLastBleat = 0f;
+			}
+
 			PhysicsBody.Velocity = (Vector3.Up + WorldRotation.Forward) * ProjectileSpeed;
-
-	}
-
-	public void OnCollisionStop( CollisionStop other )
-	{
-		Model.Set( "grounded", false );
+		}
 	}
 }
